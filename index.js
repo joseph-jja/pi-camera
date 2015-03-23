d58e6151b411160283f89649ca580edd56d0304e
joseph-jja <josepha48@yahoo.com>
2015-03-22 19:38:13 -0700

    beautify

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var Gpio = require('onoff').Gpio,
-  sensorPin = 23,
-  fs = require('fs'),
-  pir = new Gpio(sensorPin, 'in', 'both'), 
-  Mailer = require('./mailer'), 
-  args, 
-  videoList = {},
-  isRec = false, 
-  mailOptions, 
-  Sendmail, 
+    sensorPin = 23,
+    fs = require('fs'),
+    pir = new Gpio(sensorPin, 'in', 'both'),
+    Mailer = require('./mailer'),
+    args, videoList = {},
+    isRec = false,
+    mailOptions, Sendmail,
 
-args = process.argv;
+    args = process.argv;
 
 // read the config for the node mailer from the fs
 // we want sync here because it is starting up and don't want to mail anyway!
@@ -17,73 +15,72 @@ mailOptions = JSON.parse(fs.readFileSync(args[2]));
 
 Sendmail = new Mailer();
 
-Sendmail.setupTransport(mailOptions.email.host, 
-    mailOptions.email.port, 
-    mailOptions.email.auth.user, 
-    mailOptions.email.auth.pass);
+Sendmail.setupTransport(mailOptions.email.host, mailOptions.email.port, mailOptions.email.auth.user, mailOptions.email.auth.pass);
 
 Sendmail.on("start", function(data) {
-  videoList[ data.filename ] = { status: 'sending' };
+    videoList[data.filename] = {
+        status: 'sending'
+    };
 });
 
 Sendmail.on("end", function(data) {
-  var x;
-  if ( data.error ) {
-    console.log( "An error has occured: " + data.error );
-  } else {
-    // message has been sent
-    console.log( "Email status: " + info );
-    // find next message to send
-    for ( x in videoList ) { 
-      if ( videList[x] && videoList[x].status === 'unsent' ) {
-        Sendmail.sendEmail(mailOptions.user, videList[x]);
-        break;
-      } 
+    var x;
+    if(data.error) {
+        console.log("An error has occured: " + data.error);
+    } else {
+        // message has been sent
+        console.log("Email status: " + info);
+        // find next message to send
+        for(x in videoList) {
+            if(videList[x] && videoList[x].status === 'unsent') {
+                Sendmail.sendEmail(mailOptions.user, videList[x]);
+                break;
+            }
+        }
+        videoList[data.filename] = undefined;
     }
-    videoList[ data.filename ] = undefined;
-  }
 });
 
 function watchCB(err, value) {
-  var cmd, exec, 
-    videoPath, mpegPath,
-    timestamp;
-    
-  if (err) {
-    exit();
-  }
+    var cmd, exec, videoPath, mpegPath, timestamp;
 
-  if (value == 1 && !isRec) {
+    if(err) {
+        exit();
+    }
 
-    console.log('capturing video.. ');
+    if(value == 1 && !isRec) {
 
-    isRec = true;
-    
-    timestamp = new Date();
-    
-    exec = require('child_process').exec;
-    videoPath = '/tmp/video_' + timestamp.getDay() + "_" + timestamp.getHours() + '.h264';
-    mpegPath = videoPath.replace('.h264', '.mpeg');
+        console.log('capturing video.. ');
 
-    // we don't want a preview, we want video 800x600 because we are emailing
-    // we want exposure to auto for when it is dark 
-    // fps we want low also for email
-    cmd = 'raspivid -n --exposure auto -w 800 -h 600 -fps 15 -o ' + videoPath + ' -t 10000';
-    exec(cmd, function(error, stdout, stderr) {
-      // output is in stdout
-      console.log('Video Saved @ : ', videoPath);
-      // rename file to be named mpeg
-      fs.rename(videoPath, mpegPath, function(err) {
-        // no videos pending to be sent 
-        if ( videoList.length <= 0) {
-          // mail first one
-          Sendmail.sendEmail(mailOptions.user, mpegPath);
-        }
-        videoList[ mpegPath ] = { status: 'unsent' };
-        isRec = false;
-      });
-    });
-  } 
+        isRec = true;
+
+        timestamp = new Date();
+
+        exec = require('child_process').exec;
+        videoPath = '/tmp/video_' + timestamp.getDay() + "_" + timestamp.getHours() + '.h264';
+        mpegPath = videoPath.replace('.h264', '.mpeg');
+
+        // we don't want a preview, we want video 800x600 because we are emailing
+        // we want exposure to auto for when it is dark 
+        // fps we want low also for email
+        cmd = 'raspivid -n --exposure auto -w 800 -h 600 -fps 15 -o ' + videoPath + ' -t 10000';
+        exec(cmd, function(error, stdout, stderr) {
+            // output is in stdout
+            console.log('Video Saved @ : ', videoPath);
+            // rename file to be named mpeg
+            fs.rename(videoPath, mpegPath, function(err) {
+                // no videos pending to be sent 
+                if(videoList.length <= 0) {
+                    // mail first one
+                    Sendmail.sendEmail(mailOptions.user, mpegPath);
+                }
+                videoList[mpegPath] = {
+                    status: 'unsent'
+                };
+                isRec = false;
+            });
+        });
+    }
 }
 pir.watch(watchCB);
 
@@ -91,6 +88,6 @@ console.log('Pi Bot deployed successfully!');
 console.log('Guarding...');
 
 function exit() {
-  pir.unexport();
-  process.exit();
+    pir.unexport();
+    process.exit();
 }
--- mailer.js
+++ mailer.js
@@ -1,11 +1,10 @@
-var nodemailer = require('nodemailer'), 
-  transporter, 
-  util = require("util"),
-  events = require("events"),
-  timerId;
+var nodemailer = require('nodemailer'),
+    transporter, util = require("util"),
+    events = require("events"),
+    timerId;
 
 function Mailer() {
-  events.EventEmitter.call(this);
+    events.EventEmitter.call(this);
 }
 
 util.inherits(Mailer, events.EventEmitter);
@@ -13,57 +12,66 @@ util.inherits(Mailer, events.EventEmitter);
 Mailer.prototype.waitTime = 10000;
 
 Mailer.prototype.setupTransport = function(host, port, user, pass) {
-  var options = {
-    secure: false,
-    host: host,
-    port: port,
-    auth: {
-      user: user,
-      pass: pass
-    }
-  };
-  transporter = nodemailer.createTransport(options);
+    var options = {
+        secure: false,
+        host: host,
+        port: port,
+        auth: {
+            user: user,
+            pass: pass
+        }
+    };
+    transporter = nodemailer.createTransport(options);
 }
-  
+
 Mailer.prototype.sendEmail = function(user, file) {
-  var mailOptions;
-  
-  if (timerId) {
-    return;
-  }
-  this.emit("start", { "filename": file, "msg": "Sending an Email.." } );
-  
-  timerId = setTimeout(function() {
-    clearTimeout(timerId);
-    timerId = null;
-  }, this.waitTime);
+    var mailOptions;
 
-  console.log('Sending an Email..');
-  this.emit("timerSet", { "filename": file, "time": this.waitTime } );
-
-  mailOptions = {
-    from: user,
-    to: user,
-    subject: 'Motion Detected',
-    text: 'Motion detected. '
-  };
-
-  if ( file ) {
-    mailOptions.attachments = [{
-      path: file
-    }];
-  }
-
-  //console.log('DEBUG -- Sendig an Email..' + transporter.sendMail);
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log("An error occured: " + error);
-    } else {
-      console.log('Message sent: ' + info.response);
+    if(timerId) {
+        return;
     }
-    this.emit("end", { "error": error, "info": info, "filename": file } );
-  });
+    this.emit("start", {
+        "filename": file,
+        "msg": "Sending an Email.."
+    });
+
+    timerId = setTimeout(function() {
+        clearTimeout(timerId);
+        timerId = null;
+    }, this.waitTime);
+
+    console.log('Sending an Email..');
+    this.emit("timerSet", {
+        "filename": file,
+        "time": this.waitTime
+    });
+
+    mailOptions = {
+        from: user,
+        to: user,
+        subject: 'Motion Detected',
+        text: 'Motion detected. '
+    };
+
+    if(file) {
+        mailOptions.attachments = [{
+            path: file
+        }];
+    }
+
+    //console.log('DEBUG -- Sendig an Email..' + transporter.sendMail);
+    transporter.sendMail(mailOptions, function(error, info) {
+        if(error) {
+            console.log("An error occured: " + error);
+        } else {
+            console.log('Message sent: ' + info.response);
+        }
+        this.emit("end", {
+            "error": error,
+            "info": info,
+            "filename": file
+        });
+    });
 };
 
-module.exports = Mailer; 
-
+module.exports = Mailer;
