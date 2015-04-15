8716b95a9145f01663586afc54d6605b1d4ca9ab
Joe <josepha48@gmail.com>
2015-04-15 12:06:20 -0700

    Create utilities.js

---
+++ utilities.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+
+function safeUnlink(filename) {
+  if ( filename ) {
+    // remove sent video
+    // in a perfect world we would be we cant here :( 
+    try {
+      fs.unlink( filename, function ( err ) {
+        if ( err ) {
+          console.log( err );
+        }
+      } );
+    } catch ( e ) {
+      console.log( "ERROR: + e" );
+    }
+  }
+}
+
+module.exports = { safeUnlink: safeUnlink };
