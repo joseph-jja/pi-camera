0e339e5b83d83fbe93f50d621f6ae60b5ed42f33
Joe <josepha48@gmail.com>
2015-04-15 12:12:17 -0700

    initial methods

--- leds.js
+++ leds.js
@@ -1,1 +1,28 @@
+var led, 
+  Gpio = require( 'onoff' ).Gpio,;
 
+
+function Leds(enabled, ledPin) {
+  
+  if ( enabled ) {
+    led = new Gpio( ledPin, 'out' );
+  }
+  
+}
+
+Leds.prototype.changeState = function(enabled, value) {
+  
+  if ( enabled ) {
+        if ( value === 1 ) {
+            led.write( 1, function ( err ) {
+                console.log( "On " + err );
+            } );
+        } else {
+            led.write( 0, function ( err ) {
+                console.log( "Off " + err );
+            } );
+        }
+    }
+}
+
+module.exports = Leds;
