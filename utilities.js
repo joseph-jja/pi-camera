2aae071e8693812bf2fd01774749b156f14434ea
Joe <josepha48@gmail.com>
2015-06-16 21:40:47 -0700

    compute sunrse sunset better

--- utilities.js
+++ utilities.js
@@ -2,9 +2,8 @@ var fs = require( "fs" ),
     request = require( "request" ),
     winston = require( "winston" ),
     sunData = {
-        "sunrise":"12:47:15 PM",
-        "sunset":"3:33:44 AM", 
-        "offset": 7
+        "sunrise":"5:47:15 AM",
+        "sunset":"7:33:44 PM"
     };
 
 function safeUnlink( filename ) {
@@ -28,7 +27,7 @@ function safeUnlink( filename ) {
 // cm signature function (error, response, body) 
 // callback get sunData argument
 function getSunriseSunset( lat, long, callback ) {
-    var url, now = new Date(), offset;
+    var url, now = new Date(), offset = 7;
 
     // offset in hours
     offset = now.getTimezoneOffset() / 60;
@@ -38,12 +37,24 @@ function getSunriseSunset( lat, long, callback ) {
     url = 'http://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + long + '&date=today';
 
     request( url, function ( req, res ) {
-        var data, body = res.body;
+        var data, body = res.body, up, down; 
         if ( body ) {
             data = JSON.parse( body );
-            sunData.sunrise = data.results.sunrise;
-            sunData.sunset = data.results.sunset;
-            sunData.offset = offset;
+            up = data.results.sunrise;
+            down = data.results.sunset;
+            sunData.sunrise = up;
+            sunData.sunset = down;
+            up = up.substring(0,up.indexOf(":"));
+            down = down.substring(0,down.indexOf(":"));
+
+            // for us we are - 7ish hours YMMV
+            // setup sunrise
+            now.setHours( up - offset );
+            up = now.getHours() + sunData.sunrise.substring(sunData.sunrise.indexOf(":"));
+
+            // setup sunset
+            now.setHours( down - offset );
+            down = now.getHours() + sunData.sunset.substring(sunData.sunset.indexOf(":"));
         }
         callback( sunData );
     } );
