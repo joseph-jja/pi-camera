22a93addeef5df7f233df21d4782e2dca3fa77ae
joseph-jja <josepha48@yahoo.com>
2015-06-23 20:19:27 -0700

    update client, not sure it works :(

--- client.js
+++ client.js
@@ -1,12 +1,15 @@
 var client,
-    config,
+    fs = require( "fs" ),
+    config, args,
     messenger = require( "messenger" );
 
+// get args and read config 
+args = process.argv;
 config = JSON.parse( fs.readFileSync( args[ 2 ] ) );
 
-client = messenger.createSpeaker( config.listenerPort );
-setTimout( function () {
+client = messenger.createSpeaker( config.listenPort );
+//#setTimout( function () {
     client.send( config.listenerMessage, {
         changeMode: 'please'
     } );
-}, 0 );
+//}, 0 );
