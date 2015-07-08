d752a0cb5cfce1f245ca05915f53bd845c09e7c3
joseph-jja <josepha48@yahoo.com>
2015-07-07 19:29:58 -0700

    update client code to exit normally

--- client.js
+++ client.js
@@ -14,6 +14,6 @@ setTimeout( function () {
     }, function () {
         console.log( "Done! " + JSON.stringify( arguments ) );
         // client does not exit :(
-        process.abort();
+        process.exit(0);
     } );
 }, 2000 );
