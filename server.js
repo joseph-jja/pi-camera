e974d6e3929a0d57b04d4a0d0533f89098f622cf
joseph-jja <josepha48@yahoo.com>
2015-08-04 14:06:34 -0700

    update and require key to turn off and on

--- server.js
+++ server.js
@@ -1,26 +1,44 @@
 var express = require( 'express' ),
     server,
+    secureServer,
     args = process.argv,
+    bodyParser = require( 'body-parser' ),
     winston = require( 'winston' ),
     exec = require( "child_process" ).exec,
     exphbs = require( 'express-handlebars' ),
     app = express(),
+    https = require( 'https' ),
+    http = require( 'http' ),
     fs = require( "fs" ),
+    clientCode,
+    clientConfig,
     config;
 
 app.engine( '.hbs', exphbs( {
     defaultLayout: 'baseLayout',
     extname: '.hbs'
 } ) );
+
 app.set( 'view engine', '.hbs' );
 
-// read config
-config = JSON.parse( fs.readFileSync( args[ 3 ] ) );
+// for parsing application/json
+app.use( bodyParser.json() );
+// for parsing application/x-www-form-urlencoded
+app.use( bodyParser.urlencoded( {
+    extended: true
+} ) );
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get( '/', function ( req, res ) {
+// read config
+clientCode = args[ 2 ];
+clientConfig = args[ 3 ];
+config = JSON.parse( fs.readFileSync( clientConfig ) );
+
+function sendResponse( res, toggle ) {
     var result, cmd;
-    cmd = "node " + args[ 2 ] + " " + args[ 3 ];
+    cmd = "node " + clientCode + " " + clientConfig;
+    if ( typeof toggle !== 'undefined' ) {
+        cmd = cmd + " " + toggle;
+    }
     result = exec( cmd, function ( err, stdout, stderr ) {
         var result = '';
         if ( stdout ) {
@@ -32,21 +50,37 @@ app.get( '/', function ( req, res ) {
         if ( err ) {
             result += err;
         }
-        result = result.replace( "Done!", "" );
-        result = JSON.parse( result );
-        res.render( 'index', {
-            "currentState": result[ config.replyMessage ]
-        } );
+        if ( result ) {
+            result = result.replace( "Done!", "" );
+            result = JSON.parse( result );
+            res.render( 'index', {
+                "currentState": result[ config.replyMessage ]
+            } );
+        } else {
+            res.render( 'index', {
+                "currentState": "error"
+            } );
+        }
     } );
+}
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get( '/', function ( req, res ) {
+    sendResponse( res );
 } );
 
-server = app.listen( 3000, function () {
-
-    var host, port;
-
-    host = server.address().address;
-    port = server.address().port;
-
-    winston.log( 'info', 'Listening at http://%s:%s', host, port );
-
+// deal wtih a post
+app.post( '/update', function ( req, res ) {
+    var updateKey;
+    winston.log( 'info', "Key = " + req.body.changeModeKey );
+    if ( req.body.changeModeKey && req.body.changeModeKey == config.changeModeKey ) {
+        updateKey = req.body.changeModeKey;
+    }
+    sendResponse( res, updateKey );
 } );
+
+server = http.createServer( app );
+server.listen( 5000 );
+winston.log( 'info', "Listening on port 5000, not secure!" );
+//secureServer = https.createServer(options, app);
+//secureServer.listen(5443);
