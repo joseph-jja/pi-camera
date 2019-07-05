42f3cf17e01e96ca4d274d9091b89d5c7d4fd68d
Joe Acosta <josepha48@gmail.com>
2019-07-05 15:06:50 -0700

    allow passing subject, message and secure or not secure flag

--- mailer.js
+++ mailer.js
@@ -1,17 +1,17 @@
-var nodemailer = require( 'nodemailer' ),
-    winston = require( "winston" ),
-    transporter, util = require( "util" ),
-    events = require( "events" );
+var nodemailer = require('nodemailer'),
+    winston = require("winston"),
+    transporter, util = require("util"),
+    events = require("events");
 
 function Mailer() {
-    events.EventEmitter.call( this );
+    events.EventEmitter.call(this);
 }
 
-util.inherits( Mailer, events.EventEmitter );
+util.inherits(Mailer, events.EventEmitter);
 
-Mailer.prototype.setupTransport = function ( host, port, user, pass ) {
+Mailer.prototype.setupTransport = function(host, port, user, pass, opts = {}) {
     var options = {
-        secure: false,
+        secure: opts.secure || false,
         host: host,
         port: port,
         auth: {
@@ -19,47 +19,50 @@ Mailer.prototype.setupTransport = function ( host, port, user, pass ) {
             pass: pass
         }
     };
-    transporter = nodemailer.createTransport( options );
+    transporter = nodemailer.createTransport(options);
 }
 
-Mailer.prototype.sendEmail = function ( user, file ) {
+Mailer.prototype.sendEmail = function(user, file, opts = {}) {
     var mailOptions, self = this,
         timenow;
 
-    this.emit( "start", {
+    this.emit("start", {
         "filename": file,
         "msg": "Sending an Email.."
-    } );
+    });
 
-    winston.log( "info", 'Sending an Email..' );
+    winston.log("info", 'Sending an Email..');
     timenow = new Date();
+
+    const subject = opts.subject || 'Motion Detected ' + timenow.toDateString(),
+        msgText = opts.msg || 'Motion detected at ' + timenow.toString();
 
     mailOptions = {
         from: user,
         to: user,
-        subject: 'Motion Detected ' + timenow.toDateString(),
-        text: 'Motion detected at ' + timenow.toString()
+        subject: subject,
+        text: msgText
     };
 
-    if ( file ) {
-        mailOptions.attachments = [ {
+    if (file) {
+        mailOptions.attachments = [{
             path: file
-        } ];
+        }];
     }
 
-    winston.log( "debug", 'DEBUG -- Sendig an Email..' + transporter.sendMail );
-    transporter.sendMail( mailOptions, function ( error, info ) {
-        if ( error ) {
-            winston.log( "error", "An error occured: " + error );
+    winston.log("debug", 'DEBUG -- Sendig an Email..' + transporter.sendMail);
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            winston.log("error", "An error occured: " + error);
         } else {
-            winston.log( "info", 'Message sent: ' + info.response );
+            winston.log("info", 'Message sent: ' + info.response);
         }
-        self.emit( "end", {
+        self.emit("end", {
             "error": error,
             "info": info,
             "filename": file
-        } );
-    } );
+        });
+    });
 };
 
 module.exports = Mailer;
