bdb1f10048ede67bd045596f2c53a38df020ba24
Joe <josepha48@yahoo.com>
2019-07-06 14:15:21 -0700

    add in base for upgrading winston

---
+++ libs/logger.js
@@ -0,0 +1,62 @@
+const path = require( 'path' ),
+    winston = require( 'winston' );
+
+const baseDir = process.cwd();
+
+const {
+    getEnvVar
+} = require( `${baseDir}/libs/env` );
+
+const LOG_LEVEL = getEnvVar( 'LOG_LEVEL', 'info' );
+
+const MONTH_SHORT_NAMES = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
+    WEEKDAY_SHORT_NAMES = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+
+function printTimestamp() {
+    const now = new Date();
+
+    let hours = now.getHours();
+
+    const minutes = now.getMinutes(),
+        seconds = now.getSeconds(),
+        milli = now.getMilliseconds();
+
+    const weekday = WEEKDAY_SHORT_NAMES[ now.getDay() ],
+        month = MONTH_SHORT_NAMES[ now.getMonth() ];
+
+    return `${weekday} ${month} ${now.getDate()} ${now.getFullYear()} ${hours}:${minutes}:${seconds},${milli}`;
+}
+
+function createLogger( filename ) {
+    const logFormat = winston.format.combine(
+        winston.format.splat(),
+        winston.format.printf( info => {
+            return `
+    $ {
+        printTimestamp()
+    } - $ {
+        path.basename( filename )
+    } - $ {
+        info.level
+    }: $ {
+        info.message
+    }
+    `;
+        } )
+    );
+
+    // TODO handle errors?
+    const Logger = winston.createLogger( {
+        level: LOG_LEVEL,
+        exitOnError: false,
+        emitErrs: false,
+        transports: [
+            new winston.transports.Console( {
+                format: logFormat
+            } )
+        ]
+    } );
+    return Logger;
+}
+
+module.exports = createLogger;
