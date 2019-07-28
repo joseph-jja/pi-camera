616fe8d707717f0cc3d68d64e841e5ff744de677
Joe <josepha48@yahoo.com>
2019-07-27 17:43:16 -0700

    add capture python script to get 5 seconds before capturing

---
+++ capture.py
@@ -0,0 +1,25 @@
+import io
+import random
+import time
+import picamera
+
+def motion_detected():
+    # Randomly return True (like a fake motion detection routine)
+    return random.randint(0, 10) == 0
+
+camera = picamera.PiCamera()
+stream = picamera.PiCameraCircularIO(camera, seconds=5)
+camera.start_recording(stream, format='h264')
+try:
+    while True:
+        camera.wait_recording(1)
+        if motion_detected():
+            now = time.localtime()
+            videoPathBase = "/tmp/video_" + str(now.tm_hour) + "_" + str(now.tm_min) + "_" + str(now.tm_sec)
+            videoPath = videoPathBase + '.h264';
+            # Keep recording for 10 seconds and only then write the
+            # stream to disk
+            camera.wait_recording(15)
+            stream.copy_to(videoPath)
+finally:
+    camera.stop_recording()
