652f14a491584f8be2800c5b9c5602d70234af26
Joe <josepha48@gmail.com>
2020-02-15 12:55:32 -0800

    remove request dependency and add some manual testing of codes

---
+++ tests/utilities.test.js
@@ -0,0 +1,9 @@
+const baseDir = process.cwd();
+
+const {
+    getSunriseSunset
+} = require(`${baseDir}/utilities`);
+
+getSunriseSunset(38.5816, 121.4944, 4, data => {
+    console.log(data);
+});
--- utilities.js
+++ utilities.js
@@ -1,10 +1,16 @@
-const fs = require("fs"),
+const fs = require('fs'),
     baseDir = __dirname,
-    request = require("request"),
-    logger = require(`${baseDir}/libs/logger`)(__filename),
-    sunData = {
-        "sunrise": "5:47:15 AM",
-        "sunset": "7:33:44 PM"
+    httpGet = require(`${baseDir}/libs/httpGet`),
+    logger = require(`${baseDir}/libs/logger`)(__filename);
+
+let sunData = {
+    'sunrise': '5:47:15 AM',
+    'sunset': '7:33:44 PM'
+};
+
+const API_HOST = 'api.sunrise-sunset.org',
+    API_PATH = (lat, lng) => {
+        return '/json?lat=' + lat + '&lng=' + lng + '&date=today';
     };
 
 function safeUnlink(filename) {
@@ -17,52 +23,57 @@ function safeUnlink(filename) {
                     logger.error(err);
                     return;
                 }
-                logger.info("Unlinked : " + filename);
+                logger.info('Unlinked : ' + filename);
             });
         } catch (e) {
-            logger.error("ERROR: + e");
+            logger.error('ERROR: ' + e);
         }
     }
 }
 
 // cm signature function (error, response, body) 
 // callback get sunData argument
-function getSunriseSunset(lat, long, callback) {
-    var url, now = new Date(),
-        offset = 7;
+function getSunriseSunset(lat, lng, gmtOffset, callback) {
 
-    // offset in hours
-    offset = now.getTimezoneOffset() / 60;
+    let url,
+        now = new Date(),
+        offset = gmtOffset || 4;
 
     // yyyymmdd
-    url = 'http://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + long + '&date=today';
+    urlPath = API_PATH(lat, lng);
 
-    request(url, function(req, res) {
-        var data, body = res.body,
-            up, down;
-        if (body) {
-            data = JSON.parse(body);
-            up = data.results.sunrise;
-            down = data.results.sunset;
-            sunData.sunrise = up;
-            sunData.sunset = down;
-            up = up.substring(0, up.indexOf(":"));
-            down = down.substring(0, down.indexOf(":"));
+    httpGet(API_HOST, urlPath)
+        .then(body => {
+            const data = JSON.parse(body);
+            let up = data.results.sunrise;
+            let down = data.results.sunset;
+
+            sunData = {
+                sunrise: up,
+                sunset: down
+            };
+
+            up = up.substring(0, up.indexOf(':'));
+            down = down.substring(0, down.indexOf(':'));
 
             // for us we are - 7ish hours YMMV
             // setup sunrise
             now.setHours(up - offset);
-            up = now.getHours() + sunData.sunrise.substring(sunData.sunrise.indexOf(":"));
+            up = now.getHours() + sunData.sunrise.substring(sunData.sunrise.indexOf(':'));
 
             // setup sunset
             now.setHours(down - offset);
             down = now.getHours() + sunData.sunset.substring(sunData.sunset.indexOf(":"));
 
-            sunData.sunrise = up.replace("PM", "AM");
-            sunData.sunset = down.replace("AM", "PM");;
-        }
-        callback(sunData);
-    });
+            sunData.sunrise = up.replace('PM', 'AM');
+            sunData.sunset = down.replace('AM', 'PM');
+
+            callback(sunData);
+
+        }).catch(e => {
+            console.error(e);
+            callback(sunData);
+        });
 }
 
 function getDefaultSunriseSunset() {
