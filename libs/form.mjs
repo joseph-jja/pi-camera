a0f3bb1028f875fe5c616de624cedee211cad695
Joe <josepha48@gmail.com>
2022-02-21 16:38:59 -0800

    beautify form.mjs

--- libs/form.mjs
+++ libs/form.mjs
@@ -1,4 +1,6 @@
-import { EOL } from 'os';
+import {
+    EOL
+} from 'os';
 
 export function buildSelect(name, paramName, values) {
 
@@ -6,16 +8,16 @@ export function buildSelect(name, paramName, values) {
         return `<option value="${paramName} ${item}">${item}</option>`;
     }).reduce((acc, next) => {
         return `${acc}${EOL}${next}`;
-    });    
+    });
 
     return `<select name="${name}">${options}</select>`;
 }
 
 export function getRangeValues(range, step, decimalPlaces = 0) {
 
-    const values = [];    
-    for ( let i = range[0], end = range[1]; i < end; i+= +step ) {
-       values.push(i.toFixed(decimalPlaces)); 
+    const values = [];
+    for (let i = range[0], end = range[1]; i < end; i += +step) {
+        values.push(i.toFixed(decimalPlaces));
     }
     return values;
 }
@@ -27,4 +29,3 @@ export function textField(name, defaultValue = '', size = 35) {
 export function checkboxField(name, value, isEnabled = false) {
     return `<input type="checkbox" name="${name}" value="${value}">`;
 }
-
