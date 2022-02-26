c7def261733cb170de4362641187f023e94667f5
Joe <josepha48@gmail.com>
2022-02-26 12:43:46 -0800

    start processing form options

--- js/captureClient.js
+++ js/captureClient.js
@@ -4,8 +4,18 @@ window.addEventListener('DOMContentLoaded', () => {
         const target = event.target;
         const name = target.nodeName;
         if (name.toLowerCase() === 'button') {
-             const formElements = document.forms['cameraOptions'];
-             console.log(formElements);
+             const formElements = Array.from(document.forms['cameraOptions']);
+             const options = formElements.filter(element => {
+                 const nodeName = element.nodeName.toLowerCase();
+                 return (nodeName !== 'button');
+             }).map(element => {
+                 const tagName = element.tagName.toLowerCase();
+                 if (tagName === 'select') {
+                     console.log(element.selectedValue);
+                 }
+
+                 return element;                 
+             });
         }
     });
 });
