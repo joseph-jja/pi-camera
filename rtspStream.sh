ca8c35a6198edc464371225448a4196e0d042a01
Joe <josepha48@gmail.com>
2022-02-27 11:53:04 -0800

    kill lib camera and vlc if exists

--- rtspStream.sh
+++ rtspStream.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+kill -9 `ps -ef |grep libcamera-vid | awk '{print $2}'`
+kill -9 `ps -ef |grep vlc | awk '{print $2}'`
+
 EXTRA_ARGS=""
 while [[ $# -gt 0 ]]; do
     EXTRA_ARGS="$EXTRA_ARGS $1"
