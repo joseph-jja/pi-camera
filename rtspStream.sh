a6fa7a4cb30e8cc590c9f9549067f394b218e041
Joe <josepha48@gmail.com>
2022-02-27 11:10:21 -0800

    add in script for rtsp streaming

---
+++ rtspStream.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+EXTRA_ARGS=""
+while [[ $# -gt 0 ]]; do
+    EXTRA_ARGS="$EXTRA_ARGS $1"
+    shift
+done
+/usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | cvlc \
+    stream:///dev/stdin --sout '#rtp{sdp=rtsp://:10000/stream1}' :demux=h264
