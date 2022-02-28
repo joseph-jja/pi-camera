092cea6a9be99c6e355138d2322dbf0582ffeda3
Joe <josepha48@gmail.com>
2022-02-27 17:06:31 -0800

    script updates

--- capture.js
+++ capture.js
@@ -12,7 +12,7 @@ let videoProcess,
     streamProcess;
 
 const BASH_CMD = '/bin/bash';
-const VIDEO_CMD = /home/pi/pi-camera/rtspStream.sh';
+const VIDEO_CMD = '/home/pi/pi-camera/rtspStream.sh';
 
 /*
 // ffmpeg -v verbose -i rtmp://192.168.50.100:10000/<stream> 
