5141a115ac5e7d3496ae1bc7f45a54781a8467db
Joe Acosta <joe.acosta@sephora.com>
2022-02-28 17:58:52 -0800

    update mjpeg

--- mjpegRestream.sh
+++ mjpegRestream.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+LIBCAMERA_PID=`ps -ef |grep ffmpeg | grep -v grep | awk '{print $2}'`
+if [ "$LIBCAMERA_PID" != "" ] ; then
+     kill -9 "$LIBCAMERA_PID"
+fi
+
 ffmpeg -i "rtsp://192.168.50.100:10000/channel=1&stream=0.sdp" -c:v mjpeg -q:v 1 -f mpjpeg -an -
