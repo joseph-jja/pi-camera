126b17cbe5de4ba6f4f7289685499c09fa53b8cf
Joe Acosta <joe.acosta@sephora.com>
2022-03-01 15:05:56 -0800

    update ffmpeg script

--- mjpegRestream.sh
+++ mjpegRestream.sh
@@ -5,6 +5,27 @@ if [ "$LIBCAMERA_PID" != "" ] ; then
      kill -9 "$LIBCAMERA_PID"
 fi
 
+WIDTH=""
+HEIGHT=""
+EXTRA_ARGS=""
+while [[ $# -gt 0 ]]; do
+    IN_ARGS="$1"
+    if [ "$IN_ARGS" == "--width" ]; then 
+        shift
+        WIDTH=$1 
+    elif [ "$IN_ARGS" == "--height" ]; then 
+        shift
+        HEIGHT=$1 
+    fi
+    shift
+done
+
+if [ "$WIDTH" != "" ]; then 
+    if [ "$HEIGHT" != "" ] ; then 
+        EXTRA_ARGS="$EXTRA_ARGS -s $WIDTHx$HEIGHT"
+    fi
+fi
+
 IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
 ffmpeg -i "rtsp://$IP_ADDRESS:10000/stream1" \
-    -c:v mjpeg -q:v 1 -f mpjpeg -an -
+    $EXTRA_ARGS -c:v mjpeg -q:v 1 -f mpjpeg -an -
