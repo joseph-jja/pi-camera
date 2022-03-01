fd105fad984ad6e4b2ecfa2e5f07ca68812cec8f
Joe Acosta <joe.acosta@sephora.com>
2022-02-28 18:02:07 -0800

    update mjpeg

--- mjpegRestream.sh
+++ mjpegRestream.sh
@@ -5,4 +5,6 @@ if [ "$LIBCAMERA_PID" != "" ] ; then
      kill -9 "$LIBCAMERA_PID"
 fi
 
-ffmpeg -i "rtsp://192.168.50.100:10000/channel=1&stream=0.sdp" -c:v mjpeg -q:v 1 -f mpjpeg -an -
+ffmpeg -i "rtsp://192.168.50.100:10000/stream1" \
+    -vf "setpts=N/30" -fflags nobuffer -flags low_delay -framedrop \
+    -c:v mjpeg -q:v 1 -f mpjpeg -an -
