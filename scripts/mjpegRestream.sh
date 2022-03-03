7b4799b8479979e8498621eac13694cf2ef810a9
Joe Acosta <joe.acosta@sephora.com>
2022-03-03 12:22:54 -0800

    dont kill ffmpeg in mpeg script

--- scripts/mjpegRestream.sh
+++ scripts/mjpegRestream.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-LIBCAMERA_PID=`ps -ef |grep ffmpeg | grep -v grep | awk '{print $2}'`
-if [ "$LIBCAMERA_PID" != "" ] ; then
-     kill -9 "$LIBCAMERA_PID"
-fi
+#LIBCAMERA_PID=`ps -ef |grep ffmpeg | grep -v grep | awk '{print $2}'`
+#if [ "$LIBCAMERA_PID" != "" ] ; then
+#     kill -9 "$LIBCAMERA_PID"
+#fi
 
 WIDTH=""
 HEIGHT=""
