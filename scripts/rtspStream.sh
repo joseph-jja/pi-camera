ec0059d69bf9efb71390ec9be96557f3fdd9e165
Joe Acosta <joe.acosta@sephora.com>
2022-03-03 09:11:34 -0800

    try ffmpeg instead of vlc

--- scripts/rtspStream.sh
+++ scripts/rtspStream.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 LIBCAMERA_PID=`ps -ef |grep libcamera-vid | grep -v grep | awk '{print $2}'`
-if [ "$LIBCAMERA_PID" != "" ] ; then 
+if [ "$LIBCAMERA_PID" != "" ] ; then
     kill -9 "$LIBCAMERA_PID"
 fi
 VLC_PID=`ps -ef |grep vlc | grep -v grep | awk '{print $2}'`
-if [ "$VLC_PID" != "" ] ; then 
+if [ "$VLC_PID" != "" ] ; then
     kill -9 "$VLC_PID"
 fi
 
@@ -17,5 +17,7 @@ done
 
 IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
 echo "Runningscript ... IP Address: $IP_ADDRESS ..."
-/usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | /usr/bin/cvlc \
-    stream:///dev/stdin --sout '#rtp{sdp=rtsp://:10000/stream1}' :demux=h264
+#/usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | /usr/bin/cvlc \
+#    stream:///dev/stdin --sout '#rtp{sdp=rtsp://:10000/stream1}' :demux=h264
+/usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | ffmpeg \
+    -i pipe: -rtsp_flags listen "rtsp://$IP_ADDRESS:10000/stream1"
