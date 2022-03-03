f831388f84814709501983189c47b1160703a2a3
Joe Acosta <joe.acosta@sephora.com>
2022-03-03 15:30:40 -0800

    update ip addresses

--- scripts/streamServer.sh
+++ scripts/streamServer.sh
@@ -25,6 +25,6 @@ done
 IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
 echo "Running script ... IP Address: $IP_ADDRESS ..."
 /usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | /usr/bin/cvlc \
-    --no-audio stream:///dev/stdin --sout '#rtp{sdp=rtsp://:10000/stream1}' :demux=h264
+    --no-audio stream:///dev/stdin --sout '#rtp{sdp=rtsp://127.0.0.1:10000/stream1}' :demux=h264
 #/usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | ffmpeg \
 #    -i pipe: -f mpegts -listen 1 "http://127.0.0.1:10000/stream1"
