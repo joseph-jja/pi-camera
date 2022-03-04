360bf82f2dbe968140af093e78100847ed192886
Joe Acosta <joe.acosta@sephora.com>
2022-03-03 18:30:50 -0800

    stream to save

---
+++ scripts/saveStream.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+WIDTH=""
+HEIGHT=""
+FRAMERATE="-filter:v fps=10"
+EXTRA_ARGS=""
+FILENAME="capture.mjpeg"
+while [[ $# -gt 0 ]]; do
+    IN_ARGS="$1"
+    if [ "$IN_ARGS" == "--width" ]; then
+        shift
+        WIDTH=$1
+    elif [ "$IN_ARGS" == "--height" ]; then
+        shift
+        HEIGHT=$1
+    elif [ "$IN_ARGS" == "--framerate" ]; then
+        shift
+        FRAMERATE=$1
+        EXTRA_ARGS="$EXTRA_ARGS -filter:v fps=$FRAMERATE"
+    elif [ "$IN_ARGS" == "--filename" ]; then
+        shift
+        FILENAME=$1
+    fi
+    shift
+done
+
+ffmpeg -i "rtsp://127.0.0.1:10000/stream1" \
+    $EXTRA_ARGS -c:v mjpeg -q:v 1 -f mpjpeg -an $FILENAME
