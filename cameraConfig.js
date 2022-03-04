f2172a65d593b97bd3fdf51467c9e1718aad8835
Joe Acosta <joe.acosta@sephora.com>
2022-03-04 13:03:59 -0800

    add default values

--- cameraConfig.js
+++ cameraConfig.js
@@ -1,72 +1,72 @@
 module.exports = [{
-        name: 'ISO_Gain',
-        paramName: '--gain',
-        range: [1, 40],
-        step: 0.5,
-        decimalPlaces: 1,
-        comment: 'Higher value is higher ISO'
-    }, {
-        name: 'videoSize',
-        paramName: '',
-        values: [
-            '--width 640 --height 480 --profile high',
-            '--width 1280 --height 720 --profile high',
-            '--width 1920 --height 1080 --profile high',
-            '--width 1640 --height 922 --profile high',
-            '--width 1640 --height 1232 --profile high'
-        ],
-        defaultvalue: '--width 640 --height 480 --profile high'
-    }, {
-        name: 'EV_compensation',
-        comment: 'higher is brighter',
-        paramName: '--ev',
-        range: [-10, 10],
-        step: 0.5,
-        decimalPlaces: 1
-    }, {
-        name: 'metering',
-        paramName: '--metering',
-        values: ['centre', 'spot', 'average'] // 'custom'
-    }, {
-        name: 'quality',
-        paramName: '--quality',
-        range: [1, 100],
-        step: 1,
-        defaultValue: 100
-    }, {
-        name: 'framerate',
-        paramName: '--framerate',
-        values: [1, 2, 4, 8, 10, 15, 30, 60, 90, 120],
-        defaultValue: 10
-    }, {
-        name: 'exposure_profile',
-        paramName: '--exposure',
-        values: ['normal', 'sport', 'long'],
-        defaultValue: 'long'
-    }, {
-        name: 'contrast',
-        paramName: '--contrast',
-        range: [0, 1],
-        step: 0.1,
-        decimalPlaces: 1
-    }, {
-        name: 'saturation',
-        paramName: '--saturation',
-        range: [0, 1],
-        step: 0.1,
-        decimalPlaces: 1
-    }, {
-        name: 'sharpness',
-        paramName: '--sharpness',
-        range: [0, 1],
-        step: 0.1,
-        decimalPlaces: 1
-    }
-    /*,{
-        name: 'brightness',
-        paramName: '--brightness',
-        range: [-1, 1],
-        step: 0.1,
-        decimalPlaces: 1
-    }*/
+    name: 'ISO_Gain',
+    paramName: '--gain',
+    range: [1, 40],
+    step: 0.5,
+    decimalPlaces: 1,
+    comment: 'Higher value is higher ISO'
+}, {
+    name: 'videoSize',
+    paramName: '',
+    values: [
+        '--width 640 --height 480 --profile high',
+        '--width 1280 --height 720 --profile high',
+        '--width 1920 --height 1080 --profile high',
+        '--width 1640 --height 922 --profile high',
+        '--width 1640 --height 1232 --profile high'
+    ],
+    defaultvalue: '--width 640 --height 480 --profile high'
+}, {
+    name: 'EV_compensation',
+    comment: 'higher is brighter',
+    paramName: '--ev',
+    range: [-10, 10],
+    step: 0.5,
+    decimalPlaces: 1
+}, {
+    name: 'metering',
+    paramName: '--metering',
+    values: ['centre', 'spot', 'average'] // 'custom'
+}, {
+    name: 'quality',
+    paramName: '--quality',
+    range: [1, 100],
+    step: 1,
+    defaultValue: '--quality 100'
+}, {
+    name: 'framerate',
+    paramName: '--framerate',
+    values: [1, 2, 4, 8, 10, 15, 30, 60, 90, 120],
+    defaultValue: '--framerate 10'
+}, {
+    name: 'exposure_profile',
+    paramName: '--exposure',
+    values: ['normal', 'sport', 'long'],
+    defaultValue: '--exposure long'
+}, {
+    name: 'contrast',
+    paramName: '--contrast',
+    range: [0, 1],
+    step: 0.1,
+    decimalPlaces: 1
+}, {
+    name: 'saturation',
+    paramName: '--saturation',
+    range: [0, 1],
+    step: 0.1,
+    decimalPlaces: 1
+}, {
+    name: 'sharpness',
+    paramName: '--sharpness',
+    range: [0, 1],
+    step: 0.1,
+    decimalPlaces: 1
+}
+/*,{
+    name: 'brightness',
+    paramName: '--brightness',
+    range: [-1, 1],
+    step: 0.1,
+    decimalPlaces: 1
+}*/
 ];
