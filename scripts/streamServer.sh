52106e2105805a5c26517a3cdd1998a54586d3d8
Joe Acosta <joe.acosta@sephora.com>
2022-03-06 09:58:56 -0800

    fix double framerate arg

--- scripts/streamServer.sh
+++ scripts/streamServer.sh
@@ -32,8 +32,6 @@ IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
 echo "Running script ... "
 echo "IP Address: $IP_ADDRESS ..."
 echo "Options: $EXTRA_ARGS ..."
-#/usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline --listen -o tcp://192.168.50.100:10000
+
 /usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | /usr/bin/cvlc \
     --no-audio stream:///dev/stdin --sout '#rtp{sdp=rtsp://127.0.0.1:10000/stream1}' :demux=h264
-#/usr/bin/libcamera-vid $EXTRA_ARGS --nopreview -t 0 --inline -o - | ffmpeg \
-#    -i pipe: -f mpegts -listen 1 "http://127.0.0.1:10000/stream1"
