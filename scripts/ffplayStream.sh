cec02eb8a08d1a9dac9fd47608709e9cd5f92f46
Joe Acosta <joe.acosta@sephora.com>
2022-03-07 11:33:49 -0800

    parameterize ip address

--- scripts/ffplayStream.sh
+++ scripts/ffplayStream.sh
@@ -1,5 +1,9 @@
+#! /bin/sh
 
+IP_ADRESS="192.168.50.100"
+if [ "$1" != "" ]; then
+    IP_ADRESS="$1"
+fi
 
-ffplay -i "rtsp://192.168.50.100:10000/stream1" \
+ffplay -loglevel quiet -i "rtsp://$IP_ADRESS:10000/stream1" \
     -vf "setpts=N/30" -fflags nobuffer -flags low_delay -framedrop
-
