86eab63c474652f28d80d487e18154e3c99e6322
Joe <josepha48@gmail.com>
2022-03-20 20:25:58 -0700

    pipe to ffmpeg

--- scripts/directStream.sh
+++ scripts/directStream.sh
@@ -15,11 +15,13 @@ while [[ $# -gt 0 ]]; do
     EXTRA_ARGS="$EXTRA_ARGS $IN_ARGS"
     shift
 done
-
+ 
 # use tcp and avoice vlc
 IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
 #echo "Running script ... "
 #echo "IP Address: $IP_ADDRESS ..."
 #echo "Options: $EXTRA_ARGS ..."
 
-/usr/bin/libcamera-vid --codec mjpeg $EXTRA_ARGS --nopreview -t 0 --inline -o - 
+/usr/bin/libcamera-vid --codec mjpeg $EXTRA_ARGS --nopreview -t 0 --inline -o - \
+    ffmpeg -i  pipe: -filter_threads 1 -s 640x480 -filter:v fps=10 \
+    -c:v mjpeg -q:v 1 -f mpjpeg -an -
