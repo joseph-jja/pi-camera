6f9a62fc3dbec0ddcba0f5c729ddffaf43ff39bf
Joe Acosta <joe.acosta@sephora.com>
2022-03-22 13:31:14 -0700

    kill ffmpeg

--- scripts/directStream.sh
+++ scripts/directStream.sh
@@ -2,15 +2,15 @@
 
 LIBCAMERA_PID=`ps -ef |grep libcamera-vid | grep -v grep | awk '{print $2}'`
 if [ "$LIBCAMERA_PID" != "" ] ; then
-    kill -9 "$LIBCAMERA_PID"
+    kill -9 $LIBCAMERA_PID
 fi
 VLC_PID=`ps -ef |grep vlc | grep -v grep | awk '{print $2}'`
 if [ "$VLC_PID" != "" ] ; then
-    kill -9 "$VLC_PID"
+    kill -9 $VLC_PID
 fi
 FFMPPEG_PID=`ps -ef |grep ffmpeg | grep -v grep | awk '{print $2}'`
 if [ "$FFMPPEG_PID" != "" ] ; then
-    kill -9 "$FFMPPEG_PID"
+    kill -9 $FFMPPEG_PID
 fi
 
 EXTRA_ARGS=""
