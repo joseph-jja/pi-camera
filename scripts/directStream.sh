994abcf2e4e0b05f10753a768e1ec39a12723a82
Joe Acosta <joe.acosta@sephora.com>
2022-03-21 21:08:18 -0700

    pass extra args

--- scripts/directStream.sh
+++ scripts/directStream.sh
@@ -15,13 +15,12 @@ while [[ $# -gt 0 ]]; do
     EXTRA_ARGS="$EXTRA_ARGS $IN_ARGS"
     shift
 done
- 
+
 # use tcp and avoice vlc
 IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
 #echo "Running script ... "
 #echo "IP Address: $IP_ADDRESS ..."
 #echo "Options: $EXTRA_ARGS ..."
 
-#/usr/bin/libcamera-vid --codec mjpeg $EXTRA_ARGS --nopreview -t 0 --inline -o - |
-/usr/bin/libcamera-vid --codec mjpeg --nopreview -t 0 --inline -o - |
+/usr/bin/libcamera-vid --codec mjpeg $EXTRA_ARGS --nopreview -t 0 --inline -o - |
     ffmpeg -i pipe: -an -filter_threads 1 -c:v copy -f mpjpeg -
