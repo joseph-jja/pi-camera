aff8473dcb1516ede184516a324f01ea581df303
Joe <josepha48@gmail.com>
2022-03-26 14:53:56 -0700

    preview updates

--- scripts/previewStream.sh
+++ scripts/previewStream.sh
@@ -2,5 +2,5 @@
  
 FOREVER_LOG="/tmp/forever.log"
 
-ffmpeg -i pipe: -an -filter_threads 1 -filter:v fps=8 -s 640x480 -f mpjpeg -
+ffmpeg -i pipe: -an -filter_threads 1 -c:v copy -s 640x480 -f mpjpeg -
 
