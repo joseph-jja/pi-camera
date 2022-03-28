49deed0d6c0d89188ffe51872cdb130a65f3f013
Joe Acosta <joe.acosta@sephora.com>
2022-03-28 11:36:20 -0700

    convert functions to arrow functions

--- js/mjpeg.js
+++ js/mjpeg.js
@@ -1,140 +1,138 @@
 // namespace MJPEG { ...
 const MJPEG = {};
 
-//(function() {
+// class Stream { ...
+MJPEG.Stream = function(args) {
+    var self = this;
+    var autoStart = args.autoStart || false;
 
-    // class Stream { ...
-    MJPEG.Stream = function(args) {
-        var self = this;
-        var autoStart = args.autoStart || false;
+    self.url = args.url;
+    self.refreshRate = args.refreshRate || 500;
+    self.onStart = args.onStart || null;
+    self.onFrame = args.onFrame || null;
+    self.onStop = args.onStop || null;
+    self.callbacks = {};
+    self.running = false;
+    self.frameTimer = 0;
 
-        self.url = args.url;
-        self.refreshRate = args.refreshRate || 500;
-        self.onStart = args.onStart || null;
-        self.onFrame = args.onFrame || null;
-        self.onStop = args.onStop || null;
-        self.callbacks = {};
-        self.running = false;
-        self.frameTimer = 0;
+    self.img = new Image();
+    if (autoStart) {
+        self.img.onload = self.start;
+    }
+    self.img.src = self.url;
 
-        self.img = new Image();
-        if (autoStart) {
-            self.img.onload = self.start;
-        }
-        self.img.src = self.url;
-
-        const setRunning = (running) => {
-            self.running = running;
-            if (self.running) {
-                self.img.src = self.url;
-                self.frameTimer = setInterval(function() {
-                    if (self.onFrame) {
-                        self.onFrame(self.img);
-                    }
-                }, self.refreshRate);
-                if (self.onStart) {
-                    self.onStart();
+    const setRunning = (running) => {
+        self.running = running;
+        if (self.running) {
+            self.img.src = self.url;
+            self.frameTimer = setInterval(() => {
+                if (self.onFrame) {
+                    self.onFrame(self.img);
                 }
-            } else {
-                self.img.src = "#";
-                clearInterval(self.frameTimer);
-                if (self.onStop) {
-                    self.onStop();
-                }
+            }, self.refreshRate);
+            if (self.onStart) {
+                self.onStart();
             }
-        };
-
-        self.start = () => {
-            setRunning(true);
-        };
-
-        self.stop = () => {
-            setRunning(false);
-        };
-    };
-
-    // class Player { ...
-    MJPEG.Player = function(canvas, url, options) {
-
-        var self = this;
-        if (typeof canvas === "string" || canvas instanceof String) {
-            canvas = document.getElementById(canvas);
-        }
-        var context = canvas.getContext("2d");
-
-        if (!options) {
-            options = {};
-        }
-        options.url = url;
-        options.onFrame = updateFrame;
-        options.onStart = function() {
-            console.log("started");
-        }
-        options.onStop = function() {
-            console.log("stopped");
-        }
-
-        self.stream = new MJPEG.Stream(options);
-
-        canvas.addEventListener("click", function() {
-            if (self.stream.running) {
-                self.stop();
-            } else {
-                self.start();
+        } else {
+            self.img.src = "#";
+            clearInterval(self.frameTimer);
+            if (self.onStop) {
+                self.onStop();
             }
-        }, false);
-
-        function scaleRect(srcSize, dstSize) {
-            var ratio = Math.min(dstSize.width / srcSize.width,
-                dstSize.height / srcSize.height);
-            var newRect = {
-                x: 0,
-                y: 0,
-                width: srcSize.width * ratio,
-                height: srcSize.height * ratio
-            };
-            newRect.x = (dstSize.width / 2) - (newRect.width / 2);
-            newRect.y = (dstSize.height / 2) - (newRect.height / 2);
-            return newRect;
-        }
-
-        function updateFrame(img) {
-            var srcRect = {
-                x: 0,
-                y: 0,
-                width: img.naturalWidth,
-                height: img.naturalHeight
-            };
-            var dstRect = scaleRect(srcRect, {
-                width: canvas.width,
-                height: canvas.height
-            });
-            try {
-                context.drawImage(img,
-                    srcRect.x,
-                    srcRect.y,
-                    srcRect.width,
-                    srcRect.height,
-                    dstRect.x,
-                    dstRect.y,
-                    dstRect.width,
-                    dstRect.height
-                );
-                console.log(".");
-            } catch (e) {
-                // if we can't draw, don't bother updating anymore
-                self.stop();
-                console.log("!");
-                throw e;
-            }
-        }
-
-        self.start = function() {
-            self.stream.start();
-        }
-        self.stop = function() {
-            self.stream.stop();
         }
     };
 
-//})();
+    self.start = () => {
+        setRunning(true);
+    };
+
+    self.stop = () => {
+        setRunning(false);
+    };
+};
+
+// class Player { ...
+MJPEG.Player = function(canvas, url, options) {
+
+    var self = this;
+    if (typeof canvas === "string" || canvas instanceof String) {
+        canvas = document.getElementById(canvas);
+    }
+    var context = canvas.getContext("2d");
+
+    if (!options) {
+        options = {};
+    }
+    options.url = url;
+    options.onFrame = updateFrame;
+    options.onStart = () => {
+        console.log("started");
+    };
+
+    options.onStop = () => {
+        console.log("stopped");
+    };
+
+    self.stream = new MJPEG.Stream(options);
+
+    canvas.addEventListener("click", () => {
+        if (self.stream.running) {
+            self.stop();
+        } else {
+            self.start();
+        }
+    }, false);
+
+    const scaleRect = (srcSize, dstSize) => {
+        var ratio = Math.min(dstSize.width / srcSize.width,
+            dstSize.height / srcSize.height);
+        var newRect = {
+            x: 0,
+            y: 0,
+            width: srcSize.width * ratio,
+            height: srcSize.height * ratio
+        };
+        newRect.x = (dstSize.width / 2) - (newRect.width / 2);
+        newRect.y = (dstSize.height / 2) - (newRect.height / 2);
+        return newRect;
+    };
+
+    const updateFrame = (img) => {
+        var srcRect = {
+            x: 0,
+            y: 0,
+            width: img.naturalWidth,
+            height: img.naturalHeight
+        };
+        var dstRect = scaleRect(srcRect, {
+            width: canvas.width,
+            height: canvas.height
+        });
+        try {
+            context.drawImage(img,
+                srcRect.x,
+                srcRect.y,
+                srcRect.width,
+                srcRect.height,
+                dstRect.x,
+                dstRect.y,
+                dstRect.width,
+                dstRect.height
+            );
+            console.log(".");
+        } catch (e) {
+            // if we can't draw, don't bother updating anymore
+            self.stop();
+            console.log("!");
+            throw e;
+        }
+    };
+
+    self.start = () => {
+        self.stream.start();
+    };
+
+    self.stop = () => {
+        self.stream.stop();
+    };
+};
