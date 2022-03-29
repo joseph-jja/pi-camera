aed409dc37e2a64e3c7290832bfe3f5422eabcbe
Joe Acosta <joe.acosta@sephora.com>
2022-03-29 12:30:08 -0700

    add preview log data

--- scripts/previewStream.sh
+++ scripts/previewStream.sh
@@ -2,6 +2,6 @@
 
 FOREVER_LOG="/tmp/forever.log"
 
-echo "Preview has started " >> /tmp/forever.log
+echo "Preview has started ..." >> /tmp/forever.log
 # ffmpeg -i pipe: -an -filter_threads 1 -s 640x480 -qscale:v 10 -f mpeg2video -
 ffmpeg -i pipe: -an -filter_threads 1 -s 640x480 -f mpjpeg -
