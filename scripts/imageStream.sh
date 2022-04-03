76940b9e409768da4aed6ea78c4ee030140d3072
Joe <josepha48@gmail.com>
2022-04-02 19:40:02 -0700

    add image stream

---
+++ scripts/imageStream.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+FOREVER_LOG="/tmp/forever.log"
+
+/bin/bash $HOME/pi-camera/scripts/killall.sh
+
+EXTRA_ARGS=""
+while [[ $# -gt 0 ]]; do
+    IN_ARGS="$1"
+    EXTRA_ARGS="$EXTRA_ARGS $IN_ARGS"
+    shift
+done
+
+#while $EXTRA_ARGS; do
+#    if [ "$OPT" == "--framerate" ]; then
+#
+#        shift
+#    else
+#done
+
+# use tcp and avoice vlc
+IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
+echo "Running script ... " >> $FOREVER_LOG
+echo "IP Address: $IP_ADDRESS ..." >> $FOREVER_LOG
+echo "Options: $EXTRA_ARGS ..." >> $FOREVER_LOG
+
+/usr/bin/libcamera-still $EXTRA_ARGS --nopreview -t 0 --inline -o - |
+    ffmpeg -i pipe: -an -filter_threads 1 -c:v copy -f mpjpeg -
