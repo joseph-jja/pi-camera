ceed28d97e90bfb7edd983ce6b7e6f6580686933
Joe Acosta <joe.acosta@sephora.com>
2022-04-04 11:35:01 -0700

    update and test imports

--- js/captureClient.js
+++ js/captureClient.js
@@ -1,35 +1,9 @@
-window.addEventListener('DOMContentLoaded', () => {
+import bitrate from '/js/libs/bitrate';
 
-    function setBitrate(formElements) {
-        const videoSize = formElements.filter(item => {
-            return (item.name === 'videoSize');
-        })[0];
-        if (videoSize && videoSize.selectedOptions && videoSize.selectedOptions[0].value !== '') {
-            const videoOption = videoSize.selectedOptions[0].value.split(' ');
-            const [width, height] = videoOption.filter(item => {
-                return parseInt(item);
-            });
-            const wxh = width * height;
-            let bitrate = 15000000;
-            if (wxh === 307200 || wxh === 480000) {
-                // 640 x 480
-                bitrate = 15000000;
-            } else if (wxh === 921600) {
-                // 1280 x 720
-                bitrate = 10000000;
-            } else {
-                // everything else
-                bitrate = 5000000;
-            }
-            return `--bitrate ${bitrate}`;
-        }
-        return '';
-    }
+window.addEventListener('DOMContentLoaded', () => {
 
     const videoFormObj = document.forms['videoOptions'];
     const imageFormObj = document.forms['imageOptions'];
-    const mainFormObj = document.forms['mainForm'];
-    const shutdownFormObj = document.forms['shutdown'];
     const serverMsg = document.getElementById('server-messages');
 
     function getFormOptions(formObj) {
