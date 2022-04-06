75f265b322d87a5f6506eb7e25e19c9670a8a058
Joe Acosta <joe.acosta@sephora.com>
2022-04-06 15:36:47 -0700

    update defaults

--- stillConfig.js
+++ stillConfig.js
@@ -11,7 +11,7 @@ module.exports = [{
         '--width 3840 --height 2464'
     ],
     comment: '1640x1232 uses full sensor with binning',
-    defaultvalue: '--width 1640 --height 1232'
+    defaultvalue: '--width 3840 --height 2464'
 }, {
     name: 'ISO_Gain',
     paramName: '--gain',
@@ -50,7 +50,6 @@ module.exports = [{
     name: 'shutter_speed',
     paramName: '--shutter',
     values: [1, 2, 4, 8, 10, 20, 30, 40, 50, 100, 150, 200, 250, 500, 1000, 2000, 2500, 5000, 10000],
-    defaultvalue: '--shutter 250',
     comment: 'time in ms, 1 = 1/1000, 2 = 1/500, 4 = 1/250 = 1/125 or 8ms'
 }, {
     name: 'framerate',
