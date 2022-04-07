8e5ce0f4a5c36e900ee4bf9326343e00d3689408
Joe Acosta <joe.acosta@sephora.com>
2022-04-06 18:52:12 -0700

    move to JS

---
+++ js/sockImage.js
@@ -0,0 +1,14 @@
+const image = document.getElementById('stream-image');
+const xcanvas = document.getElementById('new-player');
+const context = xcanvas.getContext('2d');
+xcanvas.width = 640;
+xcanvas.height = 480;
+context.width = xcanvas.width;
+context.height = xcanvas.height;
+const socket = io(); /* eslint-disable-line */
+socket.on('connect', () => {
+    console.log('Socket connected ', socket.id);
+});
+socket.on('image', (data) => {
+    image.src = data;
+});
