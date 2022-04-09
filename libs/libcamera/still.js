1ccd479038a4a0b1a38207e96dd2054d19b6b67c
Joe <josepha48@gmail.com>
2022-04-09 16:34:30 -0700

    pass env

--- libs/libcamera/still.js
+++ libs/libcamera/still.js
@@ -16,13 +16,17 @@ module.exports = function(resolveFileLocation) {
         // stream to stdout
         spawnOptions.push('-o');
         spawnOptions.push('-');
-        return spawn(LIBCAMERA_VIDEO, spawnOptions);
+        return spawn(LIBCAMERA_VIDEO, spawnOptions, {
+            env: process.env
+        });
     }
 
     function saveImage(options) {
         const spawnOptions = ['-r'].concat(options);
 
-        return spawn(LIBCAMERA_VIDEO, spawnOptions);
+        return spawn(LIBCAMERA_VIDEO, spawnOptions, {
+            env: process.env
+        });
     }
 
     return  {
