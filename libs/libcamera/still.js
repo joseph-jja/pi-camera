0b95e1865f66b8ae5f1e806aa5e72682905234ac
Joe <josepha48@gmail.com>
2022-04-09 15:00:50 -0700

    pass options

--- libs/libcamera/still.js
+++ libs/libcamera/still.js
@@ -2,12 +2,14 @@ const { spawn } = require('childProcess');
 
 module.exports = function(resolveFileLocation) {
 
+    const stringify = require(`${resolveFileLocation}/libs/stringify`);
+    const logger = require(`${resolveFileLocation}/libs/logger`)(__filename);
     const { LIBCAMERA_VIDEO } = require(`${resolveFileLocation}/libs/libcamera/Constants`);
 
     function streamJpeg(options) {
 
         // default image streaming options
-        const spawnOptions = ['-e', 'jpg', '-t', '0', '--timelapse', '10', '--immediate'];
+        const spawnOptions = ['-e', 'jpg', '-t', '0', '--timelapse', '10', '--immediate'].concat(options);
 
         // process input options
 
@@ -18,7 +20,7 @@ module.exports = function(resolveFileLocation) {
     }
 
     function saveImage(options) {
-        const spawnOptions = ['r'];
+        const spawnOptions = ['-r'].concat(options);
 
         return spawn(LIBCAMERA_VIDEO, spawnOptions);
     }
