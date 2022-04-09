0b75b606bc2ac0c2793e3eaeb3431d6d6839d48b
Joe <josepha48@gmail.com>
2022-04-09 16:33:29 -0700

    pass environment

--- libs/ffmpeg.js
+++ libs/ffmpeg.js
@@ -5,12 +5,16 @@ const FFMPEG = 'ffmpeg';
 const DIRECT_STREAM_OPTS = '-i pipe: -an -filter_threads 1 -c:v copy -f mpjpeg -'.split(' ');
 
 function getFfmpegStream() {
-    return spawn(FFMPEG, DIRECT_STREAM_OPTS);
+    return spawn(FFMPEG, DIRECT_STREAM_OPTS, {
+        env: process.env
+    });
 }
 
 function previewStream() {
     const spawnOptions = ['-i', 'pipe:', '-an', '-filter_threads', '1', '-s', '640x480', '-f', 'mpjpeg', '-'];
-    return spawn(FFMPEG, spawnOptions);
+    return spawn(FFMPEG, spawnOptions, {
+        env: process.env
+    });
 }
 
 module.exports = {
