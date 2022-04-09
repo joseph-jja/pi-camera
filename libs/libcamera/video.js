b6c410d9076d9cd40570f5abdcebc114e480fad1
Joe <josepha48@gmail.com>
2022-04-09 13:21:39 -0700

    video launch

--- libs/libcamera/video.js
+++ libs/libcamera/video.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 
 module.exports = function(resolveFileLocation) {
 
-    const { LIBCAMERA_STILL } = require(`${resolveFileLocation}/libs/libcamera/Constants`);
+    const { LIBCAMERA_VIDEO } = require(`${resolveFileLocation}/libs/libcamera/Constants`);
     const logger = require(`${resolveFileLocation}/libs/logger`)(__filename);
 
     function streamMjpeg(options = []) {
@@ -14,21 +14,21 @@ module.exports = function(resolveFileLocation) {
         spawnOptions.push('-o');
         spawnOptions.push('-');
         console.log('Libcamera spawn options ', spawnOptions);
-        return spawn(LIBCAMERA_STILL, spawnOptions);
+        return spawn(LIBCAMERA_VIDEO, spawnOptions);
     }
 
     function saveMjpeg(options = []) {
 
         const spawnOptions = ['--codec', 'mjpeg'];
 
-        return spawn(LIBCAMERA_STILL, spawnOptions);
+        return spawn(LIBCAMERA_VIDEO, spawnOptions);
     }
 
     function saveH264(options = []) {
 
         const spawnOptions = ['--codec', 'h264'];
 
-        return spawn(LIBCAMERA_STILL, spawnOptions);
+        return spawn(LIBCAMERA_VIDEO, spawnOptions);
     }
 
     return  {
