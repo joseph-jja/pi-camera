07d1e5136a42e9c2a595644ad3cc79ef216967c6
Joe <josepha48@gmail.com>
2022-04-09 16:22:05 -0700

    remove unused

--- scripts/previewStream.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-FOREVER_LOG="/tmp/forever.log"
-
-echo "Preview has started ..." >> /tmp/forever.log
-# ffmpeg -i pipe: -an -filter_threads 1 -s 640x480 -qscale:v 10 -f mpeg2video -
-ffmpeg -i pipe: -an -filter_threads 1 -s 640x480 -f mpjpeg -
