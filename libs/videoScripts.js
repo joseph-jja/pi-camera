eddc36881c392caaa3a6b52de547a9d4129ab8c0
Joe <josepha48@gmail.com>
2022-04-10 16:18:24 -0700

    clean up some unused code

--- libs/videoScripts.js
+++ libs/videoScripts.js
@@ -252,9 +252,6 @@ module.exports = function(resolveFileLocation) {
         }
         const running = killAllRunning();
         // stream libcamera stdout to ffmpeg stdin
-        //const streamScript = `#! /bin/bash ${os.EOL}${KILL_ALL}${os.EOL}${ streamMjpeg(spawnOptions)}${os.EOL}`;
-        //fs.writeFileSync('/tmp/videoStream.sh', streamScript);
-        //global.libcameraProcess = childProcess.spawn(BASH_CMD, ['/tmp/videoStream.sh']);
         global.libcameraProcess = streamMjpeg(spawnOptions);
         global.directStreamProcess = getFfmpegStream();
         global.libcameraProcess.stdout.pipe(global.directStreamProcess.stdin);
