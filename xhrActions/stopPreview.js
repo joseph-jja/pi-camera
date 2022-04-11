96dc0c2aa2ba508f2cc0a0163a6c4f44c36d2ac2
Joe <josepha48@gmail.com>
2022-04-10 19:04:51 -0700

    move remove listeners

--- xhrActions/stopPreview.js
+++ xhrActions/stopPreview.js
@@ -4,13 +4,13 @@ module.exports = function(resolveFileLocation) {
     const logger = require(`${resolveFileLocation}/libs/logger`)(__filename);
 
     return (request, response) => {
-        try {
+        /*try {
             childProcess.execSync(`kill -9 \`ps -ef | grep previewStream | awk '{print $2}' | grep -v grep \``);
             childProcess.exec(`kill -9 \`ps -ef | grep "filter:v fps" | awk '{print $2}' | grep -v grep \``);
             logger.info('Killed all preview processes');
         } catch (e) {
             logger.info('Nothing happened!');
-        }
+        }*/
         response.writeHead(200, {});
         response.end('Preview should have stopped.');
         logger.info('Preview should have stopped.');
