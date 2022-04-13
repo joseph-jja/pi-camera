6dcdf609ceab7d47deb9a6beb8c9ad1349dfc125
Joe Acosta <joe.acosta@sephora.com>
2022-04-13 13:56:56 -0700

    add stack trace

--- capture-server.js
+++ capture-server.js
@@ -22,6 +22,7 @@ const pageUUID = /\[\[PAGE_UUID\]\]/g;
 
 process.on('uncaughtException', (e) => {
     console.error(e);
+    console.error(e.stack);
 });
 
 const stringify = require(`${basedir}/libs/stringify`),
