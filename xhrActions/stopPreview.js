3d5f26f8ed456d9575984263affb20521d4e10ef
Joe Acosta <joe.acosta@sephora.com>
2022-04-14 18:04:48 -0700

    remove unneeded code

--- xhrActions/stopPreview.js
+++ xhrActions/stopPreview.js
@@ -1,24 +1,9 @@
 const basedir = process.cwd();
 
-const logger = require(`${basedir}/libs/logger`)(__filename),
-    {
-        getImageUpdateOptions
-    } = require(`${basedir}/libs/libcamera/video`),
-    {
-        getVideoUpdateOptions
-    } = require(`${basedir}/libs/libcamera/video`),
-    {
-        getDirectStreamProcesss,
-        getImageStreamProcess
-    } = require(`${basedir}/libs/videoScripts`);
+const logger = require(`${basedir}/libs/logger`)(__filename);
 
 module.exports = (request, response) => {
     const uuid = request.query['x-uuid'];
-    if (getDirectStreamProcesss()) {
-        getDirectStreamProcesss(getVideoUpdateOptions());
-    } else if (getImageStreamProcess()) {
-        getImageStreamProcess(getImageUpdateOptions());
-    }
     response.writeHead(200, {});
     const message = `Preview should have stopped for : ${uuid}`;
     response.end(message);
