9d29be48ad64234dbc6a84f2ff2e825502c85bbb
Joe Acosta <joe.acosta@sephora.com>
2022-04-18 15:37:12 -0700

    specify which form the option is for

--- libs/libcamera/configProfiles.js
+++ libs/libcamera/configProfiles.js
@@ -2,15 +2,19 @@ module.exports = [{
     name: 'Star Capture',
     fields: [{
         name: 'ISO_Gain',
-        value: 40.0
+        value: 40.0,
+        forms: ['videoOptions', 'imageOptions']
     }, {
         name: 'EV_compensation',
-        value: 10.0
+        value: 10.0,
+        forms: ['videoOptions', 'imageOptions']
     }, {
         name: 'framerate',
-        value: '--framerate 4'
+        value: '--framerate 4',
+        forms: ['videoOptions']
     }, {
         name: 'shutter_speed',
-        value: 250000
+        value: 250000,
+        forms: ['videoOptions', 'imageOptions']
     }]
 }];
