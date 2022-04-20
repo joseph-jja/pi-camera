f151ac0a71b94322a170e3cf4eff01b242d37213
Joe Acosta <joe.acosta@sephora.com>
2022-04-20 16:48:07 -0700

    clean up canvas stuff a bit

--- js/mjpegCapture.js
+++ js/mjpegCapture.js
@@ -9,39 +9,26 @@ import {
 } from '/js/mjpeg/index.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
-    let streamPlayer;
-
-    if (!getParamValue('canvas')) {
-        return;
-    }
 
     const canvasObj = document.getElementById('player');
-    if (!canvasObj) {
+    if (!getParamValue('canvas') || !canvasObj) {
         return;
     }
     canvasObj.style.display = 'block';
-
-    const uuid = (!xUuid) ? /* eslint-disable-line */
-        await fetch('/config').then(async resp => {
-            const headers = await resp.headers;
-            const xuid = headers.get('x-uuid');
-            return xuid;
-        }) : xUuid;  /* eslint-disable-line */
-
-    if (canvasObj.style.display === 'block') {
-        const options = {
-            errorHandler: async (err) => {
-                console.log(err);
-                //streamPlayer.stream = new MJPEGStream(streamPlayer.options);
-                //streamPlayer.start();
-            },
-            refreshRate: 250,
-            onStop: stopPreview
-        };
-
-        //Leave your .mjpeg video URL here.
-        streamPlayer = new MJPEGPlayer('player', `/preview?x-uuid=${uuid}`, options);
-        streamPlayer.start();
-        window.streamPlayer = streamPlayer;
+    if (canvasObj.style.display !== 'block') {
+        return;
     }
+
+    const options = {
+        errorHandler: async (err) => {
+            console.log(err);
+            //streamPlayer.stream = new MJPEGStream(streamPlayer.options);
+            //streamPlayer.start();
+        },
+        refreshRate: 250,
+        onStop: stopPreview
+    };
+    const streamPlayer = new MJPEGPlayer('player', `/preview?x-uuid=${window.xUuid}`, options);
+    streamPlayer.start();
+    window.streamPlayer = streamPlayer;
 });
