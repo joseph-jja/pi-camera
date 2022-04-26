5b8c1cf5e884e1ec1f92708c8fe660633d965b11
Joe Acosta <joe.acosta@sephora.com>
2022-04-25 19:08:55 -0700

    add raspi cmds

--- libs/libcamera/Constants.js
+++ libs/libcamera/Constants.js
@@ -2,7 +2,13 @@ const LIBCAMERA_STILL = '/usr/bin/libcamera-still';
 
 const LIBCAMERA_VIDEO = '/usr/bin/libcamera-vid';
 
+const RASPISTILL = '/usr/bin/raspistill';
+
+const RASPIVIDEO = '/usr/bin/raspivid';
+
 module.exports = {
     LIBCAMERA_STILL,
-    LIBCAMERA_VIDEO
+    LIBCAMERA_VIDEO,
+    RASPISTILL,
+    RASPIVIDEO
 };
