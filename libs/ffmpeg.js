b754c8afaf4be81a0f89f8669aa1dc06e4879369
Joe Acosta <joe.acosta@sephora.com>
2022-04-27 11:49:26 -0700

    add ffmpeg output webm

--- libs/ffmpeg.js
+++ libs/ffmpeg.js
@@ -5,14 +5,31 @@ const {
 const FFMPEG = 'ffmpeg';
 
 const DIRECT_STREAM_OPTS = ['-i', 'pipe:',
-    '-an',
-    '-filter_threads', '1',
-    '-r', '4',
-    '-q:v', '2',
-    '-s', '640x480',
-    '-f', 'mpjpeg', '-'];
+        '-an',
+        '-filter_threads', '1',
+        '-r', '4',
+        '-q:v', '2',
+        '-s', '640x480',
+        '-f', 'mpjpeg', '-'],
+    WEB_DIRECT_STREAM_OPTS = ['-i', 'pipe:',
+        '-an',
+        '-filter_threads', '1',
+        '-r', '4',
+        '-q:v', '2',
+        '-s', '640x480',
+        '-c:v', 'libvpx',
+        '-crf', '10',
+        '-b:v', '1M',
+        '-c:a', 'libvorbis', '-'];
 
 const defaultFramerateIndex = DIRECT_STREAM_OPTS.indexOf('-r') + 1;
+
+function getFfmpegWebmStream(framerate = 4) {
+    WEB_DIRECT_STREAM_OPTS[defaultFramerateIndex] = framerate;
+    return spawn(FFMPEG, WEB_DIRECT_STREAM_OPTS, {
+        env: process.env
+    });
+}
 
 function getFfmpegStream(framerate = 4) {
     DIRECT_STREAM_OPTS[defaultFramerateIndex] = framerate;
@@ -41,6 +58,7 @@ function playFile(filename, config) {
 }
 
 module.exports = {
+    getFfmpegWebmStream,
     getFfmpegStream,
     playFile
 };
