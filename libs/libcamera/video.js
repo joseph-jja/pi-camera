35632c36b3ce1506cc2a612df099e9815aaf91f1
Joe Acosta <joe.acosta@sephora.com>
2022-04-27 14:38:06 -0700

    add logic to find video devices

--- libs/libcamera/video.js
+++ libs/libcamera/video.js
@@ -1,6 +1,9 @@
 const {
-    spawn
-} = require('child_process');
+        readdir
+    } = require('fs'),
+    {
+        spawn
+    } = require('child_process');
 
 const basedir = process.cwd();
 
@@ -37,6 +40,64 @@ if (DEFAULT_OPTIONS.length === 0) {
         }
     });
     setVideoUpdateOptions(DEFAULT_OPTIONS);
+}
+
+function getVideoCaptureDevices() {
+    return new Promise((resolve, reject) => {
+        readdir('/dev', (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            const videoDevices = data.filter(device => {
+                return device.startsWith('video');
+            });
+            return resolve(videoDevices);
+        });
+    });
+}
+
+function runVideo4Linux2Control(device) {
+    return new Promise((resolve, reject) => {
+        const v4l2ctl= spawn('v4l2-ctl', [`--device=/dev/${device}`, '--all']);
+        const capture = spawn('grep', ['Video Capture']);
+        const filtered = spawn('grep', ['-v', 'Format Video Capture']);
+
+        let hasdata = false;
+
+	    v4l2ctl.stdout.pipe(capture.stdin);
+	    capture.stdout.pipe(filtered.stdin);
+	    filtered.stdout.on('data', d => {
+		    if (d && d.length > 0 ) {
+                hasdata = true;
+		    }
+	    });
+
+	    v4l2ctl.stderr.on('data', d => {
+            return reject(d);
+	    });
+	    capture.stderr.on('data', d => {
+            return reject(d);
+	    });
+	    filtered.stderr.on('data', d => {
+            return reject(d);
+	    });
+        filtered.on('close', () => {
+            return resolve(hasdata);
+        });
+    });
+}
+
+async function getFirstVideoCaptureDevice() {
+
+    const devices = getVideoCaptureDevices();
+
+    const promiseList = devices.map(device => {
+        return runVideo4Linux2Control(device);
+    });
+
+    const resultDevices = await Promise.allSettled(promiseList);
+
+    return resultDevices;
 }
 
 function streamMjpeg(options = []) {
