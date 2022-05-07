832fd330445b6e740e4bd468fdbeaaa07ac2f5f2
Joe <josepha48@gmail.com>
2022-05-07 12:25:36 -0700

    add some way to get supported formats

--- getDeviceInfo.js
+++ getDeviceInfo.js
@@ -7,7 +7,11 @@ const {
 const outStream = createWriteStream('/tmp/fmts.log');
 
 const command = spawn('ffmpeg', ['-f', 'video4linux2', '-list_formats', 'all', '-i', '/dev/video0']);
+const grep = spawn('grep', ['Raw']);
 
-command.stdout.pipe(outStream);
-command.stderr.pipe(outStream);
+command.stdout.pipe(grep);
+command.stderr.pipe(grep);
+
+grep.stdout.pipe(outStream);
+grep.stderr.pipe(outStream);
 
