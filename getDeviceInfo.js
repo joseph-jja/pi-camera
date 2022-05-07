76b7a61055ce37a8b9c72631b57e21649cc6bbae
Joe <josepha48@gmail.com>
2022-05-07 12:39:32 -0700

    update using gst

--- getDeviceInfo.js
+++ getDeviceInfo.js
@@ -5,13 +5,32 @@ const {
 } = require('child_process');
 
 const outStream = createWriteStream('/tmp/fmts.log');
+// set to false if you don't have gst-device-monitor-1.0 from gstreamer1.0-plugins-base-apps
+const USE_GST = true;
 
-const command = spawn('ffmpeg', ['-f', 'video4linux2', '-list_formats', 'all', '-i', '/dev/video0']);
-const grep = spawn('grep', ['Raw']);
+if (USE_GST) {
 
-command.stdout.pipe(grep.stdin);
-command.stderr.pipe(grep.stdin);
+    const command = spawn('gst-device-monitor-1.0');
+    const grep = spawn('grep', ['format']);
+    const vgrep1 = spawn('grep', ['-v', '\[']);
 
-grep.stdout.pipe(outStream);
-grep.stderr.pipe(outStream);
+    command.stdout.pipe(grep.stdin);
+    command.stderr.pipe(grep.stdin);
 
+    grep.stdout.pipe(vgrep1.stdin);
+    grep.stderr.pipe(vgrep1.stdin);
+
+    vgrep1.stdout.pipe(outStream);
+    vgrep1.stderr.pipe(outStream);
+
+} else {
+    const command = spawn('ffmpeg', ['-f', 'video4linux2', '-list_formats', 'all', '-i', '/dev/video0']);
+    const grep = spawn('grep', ['Raw']);
+
+    command.stdout.pipe(grep.stdin);
+    command.stderr.pipe(grep.stdin);
+
+    grep.stdout.pipe(outStream);
+    grep.stderr.pipe(outStream);
+
+}
