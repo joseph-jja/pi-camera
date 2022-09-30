8fc01ba742caabcc81ea868888daea95b980e810
Joe Acosta <joe.acosta@sephora.com>
2022-09-30 13:02:29 -0700

    add convert and trim scripts to project folder

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,13 +1,10 @@
-#! /bin/sh
 
-FOREVER_LOG="/tmp/forever.log"
+I=0
+for FILE in `ls *.mjpeg`; do 
 
-#/bin/bash $HOME/pi-camera/scripts/killall.sh
+    I=$((I+1))
+    BASE=`echo $FILE | sed 's/.mjpeg//g' `
+    #ffmpeg -i $FILE -r 60 images/$BASE-0$I-%4d.png
+    ffmpeg -i $BASE.mjpeg -r 30 -c copy $BASE.mp4
 
-# use tcp and avoice vlc
-IP_ADDRESS=`env |grep IP_ADDR | sed 's/IP_ADDR=//g'`
-echo "Running script ... " >> $FOREVER_LOG
-echo "IP Address: $IP_ADDRESS ..." >> $FOREVER_LOG
-echo "Options: $EXTRA_ARGS ..." >> $FOREVER_LOG
-
-fmpeg -i "$1" -o "$1.mp4"
+done
---
+++ scripts/trim.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+ffmpeg -i $1  -ss 00:00:00 -t 00:00:$2 $1.mjpeg
+mv $1.mjpeg $1
