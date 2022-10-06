a73ba7899a2ad0ca3e3cacd711cf0556a5ffea6c
Joe Acosta <joe.acosta@sephora.com>
2022-10-06 12:55:33 -0700

    more tweaks

--- libs/libcamera/gstreamer.js
+++ libs/libcamera/gstreamer.js
@@ -34,15 +34,6 @@ function gstreamer() {
             const vgrep1 = spawn('grep', ['-v', 'v4l2_videodevice']);
             const outStream = createWriteStream('/tmp/fmts.log');
 
-            command.stdout.pipe(grep.stdin);
-            command.stderr.pipe(grep.stdin);
-
-            grep.stdout.pipe(vgrep1.stdin);
-            grep.stderr.pipe(vgrep1.stdin);
-
-            vgrep1.stdout.pipe(outStream);
-            vgrep1.stderr.pipe(outStream);
-
             outStream.once('finish', () => {
                 hasRun = true;
                 data = {
@@ -52,18 +43,21 @@ function gstreamer() {
                 return resolve(data);
             });
 
+            command.stdout.pipe(grep.stdin);
+            command.stderr.pipe(grep.stdin);
+
+            grep.stdout.pipe(vgrep1.stdin);
+            grep.stderr.pipe(vgrep1.stdin);
+
+            vgrep1.stdout.pipe(outStream);
+            vgrep1.stderr.pipe(outStream);
+
         } else {
             const ffmpeg = await whichCommand('ffmpeg').catch(errorHandler);
             if (ffmpeg) {
                 const command = spawn('ffmpeg', ['-f', 'video4linux2', '-list_formats', 'all', '-i', '/dev/video0']);
                 const grep = spawn('grep', ['Raw']);
                 const outStream = createWriteStream('/tmp/fmts.log');
-
-                command.stdout.pipe(grep.stdin);
-                command.stderr.pipe(grep.stdin);
-
-                grep.stdout.pipe(outStream);
-                grep.stderr.pipe(outStream);
 
                 outStream.once('finish', () => {
                     hasRun = true;
@@ -73,6 +67,12 @@ function gstreamer() {
                     };
                     return resolve(data);
                 });
+
+                command.stdout.pipe(grep.stdin);
+                command.stderr.pipe(grep.stdin);
+
+                grep.stdout.pipe(outStream);
+                grep.stderr.pipe(outStream);
 
             } else {
                 logger.error(`Install gst-device-monitor-1.0 to get video modes`);
