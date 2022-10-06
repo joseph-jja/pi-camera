b5215c4567f88a845060f1ae121b288c2c55c19e
Joe Acosta <joe.acosta@sephora.com>
2022-10-06 09:25:25 -0700

    use built in tool

--- tools/getDeviceInfo.js
+++ tools/getDeviceInfo.js
@@ -1,37 +1,4 @@
-const {
-    createWriteStream
-} = require('fs'), {
-    spawn
-} = require('child_process');
+const basedir = process.cwd(), 
+    gstreamer = require(`${basedir}/libs/libcamera/gstreamer`);
 
-// set to false if you don't have gst-device-monitor-1.0 from gstreamer1.0-plugins-base-apps
-const USE_GST = true;
-
-if (USE_GST) {
-
-    const command = spawn('gst-device-monitor-1.0');
-    const grep = spawn('grep', ['format']);
-    const vgrep1 = spawn('grep', ['-v', 'v4l2_videodevice']);
-    const outStream = createWriteStream('/tmp/fmts.log');
-
-    command.stdout.pipe(grep.stdin);
-    command.stderr.pipe(grep.stdin);
-
-    grep.stdout.pipe(vgrep1.stdin);
-    grep.stderr.pipe(vgrep1.stdin);
-
-    vgrep1.stdout.pipe(outStream);
-    vgrep1.stderr.pipe(outStream);
-
-} else {
-    const command = spawn('ffmpeg', ['-f', 'video4linux2', '-list_formats', 'all', '-i', '/dev/video0']);
-    const grep = spawn('grep', ['Raw']);
-    const outStream = createWriteStream('/tmp/fmts.log');
-
-    command.stdout.pipe(grep.stdin);
-    command.stderr.pipe(grep.stdin);
-
-    grep.stdout.pipe(outStream);
-    grep.stderr.pipe(outStream);
-
-}
+gstreamer();
