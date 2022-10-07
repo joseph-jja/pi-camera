7f124ae384317042cb8341b983f6f2172c858233
Joe Acosta <joe.acosta@sephora.com>
2022-10-06 18:15:48 -0700

    new file for handleing modes

---
+++ libs/libcamera/modes.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const CAMERA_ID_RE = /(imx\d*) (\[\d*x\d*\])/;
+
+const CAMEERA_MODES = /(\d*x\d*) (\[\d*\.\d* fps - \(\d*, \d*\)\/\d*x\d* crop\])/;
+
+async function processModes(configFile) {
+
+    
+}
