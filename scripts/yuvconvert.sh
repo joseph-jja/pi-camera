d8fd2aa923802b4ecab71e56c3be36c5b61543ef
Joe Acosta <joe.acosta@sephora.com>
2022-10-28 15:12:40 -0700

    add in option for framerate

--- scripts/yuvconvert.sh
+++ scripts/yuvconvert.sh
@@ -2,5 +2,6 @@
 
 # $1 is size like 1920x1080
 # $2 is input file xxx without file extension
-ffmpeg -f rawvideo -vcodec rawvideo -s $1 -r 30 -pix_fmt yuv420p -i $2.yuv -c:v libx264 -preset ultrafast -qp 0 $2.mp4
+# $3 is framerate
+ffmpeg -f rawvideo -vcodec rawvideo -s $1 -r $3 -pix_fmt yuv420p -i $2.yuv -c:v libx264 -preset ultrafast -qp 0 $2.mp4
 
