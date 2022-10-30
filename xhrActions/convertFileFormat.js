99150100ae1ed4e6a29478c1bc443165b9be2fc6
Joe <josepha48@gmail.com>
2022-10-30 14:35:29 -0700

    update covert to process files

--- xhrActions/convertFileFormat.js
+++ xhrActions/convertFileFormat.js
@@ -12,6 +12,7 @@ const basdir = process.cwd(),
         convertH264,
         convertMJPEG
     } = require(`${basedir}/libs/ffmpeg`),
+    promiseWrapper = require(`${basedir}/libs/PromiseWrapper`),
     logger = require(`${basedir}/libs/logger`)(__filename);
 
 const MJPEG_EXT = '.mjpeg',
@@ -33,7 +34,11 @@ async function readConfigFile(configFile) {
 // read all the video files only
 function getFiles() {
 
-    const videoFileList = await readdir(BASE_CONFIG_PATH);
+    const [err, videoFileList] = await promiseWrapper(readdir(BASE_CONFIG_PATH));
+    
+    if (err) {
+        return [];
+    }
     
     const files = videoFileList.filter(file => {
         return (file.endsWith(MJPEG_EXT) || file.endsWith(YUV420_EXT) ||
@@ -41,6 +46,35 @@ function getFiles() {
     });
 
     return files;
+}
+
+
+function processFile(filename, config) {
+
+    return new Promise((resolve, reject) => {
+        
+        if (filename.endsWith(MJPEG_EXT)) {
+            const convert = convertMJPEG(filename, config, filename.replace(MJPEG_EXT, MP4_EXT));
+            convert.once('close', (code) => {
+                return resolve(code);
+            });
+        } else if (filename.endsWith(YUV420_EXT)) {
+            const convert = convertYUV420(filename, config, filename.replace(YUV420_EXT, MP4_EXT));
+            convert.once('close', (code) => {
+                return resolve(code);
+            });
+        } else if (filename.endsWith(H264_EXT)) {
+            const convert = convertH264(filename, config, filename.replace(H264_EXT, MP4_EXT));
+            convert.once('close', (code) => {
+                return resolve(code);
+            });
+        } else if (filename.endsWith(RAW_EXT)) {
+            logger.warn('Cannot convert RAW files at this time');
+            return resolve(-1);
+        } else {
+            return resolve(-1);
+        }
+    });
 }
 
 module.exports = (request, response) => {
@@ -56,18 +90,10 @@ module.exports = (request, response) => {
         const configFileName = `${filename}.cfg`.replace(/\/images\//, '/imageConfig/');
 
         const config = await readConfigFile(configFileName);
-
-        if (filename.endsWith(MJPEG_EXT)) {
-            convertMJPEG(filename, config, filename.replace(MJPEG_EXT, MP4_EXT));
+        
+        const [err, result] = await promiseWrapper(processFile(filename, config));
+        if (!err) {
             converted++;
-        } else if (filename.endsWith(YUV420_EXT)) {
-            convertYUV420(filename, config, filename.replace(YUV420_EXT, MP4_EXT));
-            converted++;
-        } else if (filename.endsWith(H264_EXT)) {
-            convertH264(filename, config, filename.replace(H264_EXT, MP4_EXT));
-            converted++;
-        } else if (filename.endsWith(RAW_EXT)) {
-            logger.warn('Cannot convert RAW files at this time');
         }
     });
 
