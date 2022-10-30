8a1ac0ddd80ed450cf53c976cd4c83ec07caff1c
Joe <josepha48@gmail.com>
2022-10-30 14:19:44 -0700

    enable file conversion for 3 types of files

--- xhrActions/convertFileFormat.js
+++ xhrActions/convertFileFormat.js
@@ -1,8 +1,24 @@
-const { readFile } = require('fs/promises');
-
 const {
-    convertYUV420
-} = require(`${basedir}/libs/ffmpeg`);
+    readFile,
+    readdir
+} = require('fs/promises');
+
+const basdir = process.cwd(),
+    {
+        BASE_CONFIG_PATH
+    } = = require(`${basedir}/libs/videoScripts`),
+    {
+        convertYUV420,
+        convertH264,
+        convertMJPEG
+    } = require(`${basedir}/libs/ffmpeg`),
+    logger = require(`${basedir}/libs/logger`)(__filename);
+
+const MJPEG_EXT = '.mjpeg',
+    RAW_EXT = '.raw',
+    YUV420_EXT = '.yuv420',
+    H264_EXT = '.h264', 
+    MP4_EXT = '.mp4;
 
 async function readConfigFile(configFile) {
 
@@ -14,28 +30,40 @@ async function readConfigFile(configFile) {
 
 }
 
+// read all the video files only
 function getFiles() {
 
-    return [];
+    const videoFileList = await readdir(BASE_CONFIG_PATH);
+    
+    const files = videoFileList.filter(file => {
+        return (file.endsWith(MJPEG_EXT) || file.endsWith(YUV420_EXT) ||
+            file.endsWith(H264_EXT) || file.endsWith(H264_EXT) );    
+    });
+
+    return files;
 }
 
 module.exports = (request, response) => {
+    
+    const files = await getFiles();
 
-    getFiles().forEach(async filename => {
+    files.forEach(async filename => {
 
         const configFileName = `${filename}.cfg`.replace(/\/images\//, '/imageConfig/');
 
         const config = await readConfigFile(configFileName);
 
-        if (filename.endsWith('.mjpeg')) {
-
-        } else if (filename.endsWith('.yuv420')) {
-
-        } else if (filename.endsWith('.h264')) {
-        
+        if (filename.endsWith(MJPEG_EXT)) {
+            convertMJPEG(filename, config, filename.replace(MJPEG_EXT, MP4_EXT));
+        } else if (filename.endsWith(YUV420_EXT)) {
+            convertYUV420(filename, config, filename.replace(YUV420_EXT, MP4_EXT));
+        } else if (filename.endsWith(H264_EXT)) {
+            convertH264(filename, config, filename.replace(H264_EXT, MP4_EXT));
+        } else if (filename.endsWith(RAW_EXT)) {
+            logger.warn('Cannot convert RAW files at this time');
         }
     });
-    
+
     response.writeHead(200, {});
     response.end('Conversion completed!');
 };
