fc528f95680674e3864b87b7ccdb03ca24dfaf83
Joe <josepha48@gmail.com>
2022-11-15 18:39:54 -0800

    update restore function

--- js/captureClient.js
+++ js/captureClient.js
@@ -40,12 +40,14 @@ function restore() {
                 const value = item[key];
                 const selectObj = formObj[key];
                 if (selectObj && value) {
-                    const selectOpts = selectObj.options;
-                    const index = Array.from(selectOpts).findIndex(item => {
-                        return (value === item.value);
-                    });
-                    if (index > 0) {
-                        selectObj.selectedIndex = index;
+                    const selectOpts = selectObj.options;A
+			        if (selectOpts &&Array.isArray(selectOpts)) {
+                        const index = Array.from(selectOpts).findIndex(item => {
+                            return (value === item.value);
+                        });
+                        if (index > 0) {
+                            selectObj.selectedIndex = index;
+                        }
                     }
                 }
             }
