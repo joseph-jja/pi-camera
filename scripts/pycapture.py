aeaa11e3a8a0b20a1c53627cce3f7beeaac8d6e6
Joe Acosta <joe.acosta@sephora.com>
2022-12-22 12:44:10 -0800

    update script for still capture

--- scripts/pycapture.py
+++ scripts/pycapture.py
@@ -1,6 +1,7 @@
 # deps
 # sudo apt install -y python3-libcamera python3-kms++
 # sudo apt install -y python3-prctl libatlas-base-dev ffmpeg libopenjp2-7 python3-pip
+# sudo apt install -y python3-picamera2
 # pip3 install numpy --upgrade
 # opencv?
 # sudo apt install -y python3-opencv
@@ -12,16 +13,21 @@
 # - image capture thread
 # - process and save images thread
 
+import io
 import time
-
-from picamera2 import Picamera2
-from picamera2.encoders import ??
 
 from picamera2 import Picamera2
 
 picam2 = Picamera2()
 
 #picam2.preview_configuration
-config = picam2.video_configuration(main={"size": (1332, 990), "format": "YUV420"})
+config = picam2.still_configuration.size = (1920, 1080)
+#config = picam2.still_configuration.shutter = 100
 
 picam2.configure(config)
+
+picam2.start()
+time.sleep(0.1)
+
+data = io.BytesIO()
+print(data.getbuffer().nbytes)
