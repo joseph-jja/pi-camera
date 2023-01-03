737a2f6eeb142984f1ae275cfb158989a4bcf9a6
Joe Acosta <joe.acosta@sephora.com>
2023-01-03 14:15:50 -0800

    enable convert more file types

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,10 +1,10 @@
 
 I=0
-for FILE in `ls *.mjpeg`; do 
+for FILE in `ls *.mjpeg *.avi *.h264 *.ts *.yuv`; do 
 
     I=$((I+1))
-    BASE=`echo $FILE | sed 's/.mjpeg//g' `
+    BASE=`echo $FILE | sed 's/.mjpeg//g' | sed 's/.avi//g' | sed 's/.h264//g' | sed 's/.ts//g' | sed 's/.yuv//g'  `
     #ffmpeg -i $FILE -r 60 images/$BASE-0$I-%4d.png
-    ffmpeg -i $BASE.mjpeg -r 30 -c copy $BASE.mp4
+    ffmpeg -i $FILE -r 30 -c copy $BASE.mp4
 
 done
