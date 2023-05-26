1083f4179cfcf6078c25e6ecc34d605cb8d32f02
Joe <josepha48@gmail.com>
2023-05-25 20:37:54 -0700

    update chart

--- libs/histogram.js
+++ libs/histogram.js
@@ -1,50 +1,54 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
 const sharp = require('sharp');
 
-// Read the image stream
-const imageStream = fs.readFileSync('/home/josepha48/space/Pleiades-2022-11-29-stacked_1.png');
-
-// Process the image stream
-sharp(imageStream)
-  .raw()
-  .toBuffer((err, data, info) => {
-    if (err) {
-      console.error('An error occurred while processing the image:', err);
-      return;
-    }
-
-    // Calculate the histogram
-    const histogram = calculateHistogram(data, info.channels);
-
-    // Print the histogram
-    console.log(histogram);
-  });
-
 // Function to calculate the histogram
 function calculateHistogram(data, channels) {
-  const histogram = {};
+    const histogram = {};
 
-  // Initialize the histogram bins
-  for (let i = 0; i < 256; i++) {
-    histogram[i] = 0;
-  }
+    // Initialize the histogram bins
+    for (let i = 0; i < 256; i++) {
+        histogram[i] = 0;
+    }
 
-  // Iterate over the image data and update the histogram
-  for (let i = 0; i < data.length; i += channels) {
-    // Assuming the image has RGB channels
-    const r = data[i];
-    const g = data[i + 1];
-    const b = data[i + 2];
+    // Iterate over the image data and update the histogram
+    for (let i = 0; i < data.length; i += channels) {
+        // Assuming the image has RGB channels
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
 
-    // Calculate the grayscale value
-    const grayscale = Math.floor((r + g + b) / 3);
+        // Calculate the grayscale value
+        const grayscale = Math.floor((r + g + b) / 3);
 
-    // Increment the corresponding bin in the histogram
-    histogram[grayscale]++;
-  }
+        // Increment the corresponding bin in the histogram
+        histogram[grayscale]++;
+    }
 
-  return histogram;
+    return histogram;
+}
+
+module.exports = async function getHistogram(filename) {
+
+    // Read the image stream
+    const imageStream = await fs.readFile(filename);
+
+    // Process the image stream
+    sharp(imageStream).raw().toBuffer((err, data, info) => {
+
+        return new Promise((resolve, reject) => {
+            if (err) {
+                console.error('An error occurred while processing the image:', err);
+                return reject(err);
+            }
+
+            // Calculate the histogram
+            const histogram = calculateHistogram(data, info.channels);
+
+            // Print the histogram
+            return resolve(histogram);
+        });
+    });
 }
 
 /*
