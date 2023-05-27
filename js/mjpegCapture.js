c5c8f5110a8fb615b134d6c29f43a0e728363450
Joe <josepha48@gmail.com>
2023-05-27 14:01:13 -0700

    update and get working histogram for images

--- js/mjpegCapture.js
+++ js/mjpegCapture.js
@@ -10,6 +10,16 @@ import {
 
 window.addEventListener('DOMContentLoaded', async () => {
     
+    if (window.histogramCanvasRef) {
+        const height = window.histogramCanvasRef.height;
+        const width = window.histogramCanvasRef.width;
+        const canvasHeight = height-10;
+        window.histogramCanvasRef.rectangle(0, 0, width, height, {
+            color: '#FF6347',
+            fillStrokeClear: 'fill'
+        });
+    }
+    
     const canvasObj = document.getElementById('player');
     if (!getParamValue('canvas')) {
         //not a canvas to do iframe impl
