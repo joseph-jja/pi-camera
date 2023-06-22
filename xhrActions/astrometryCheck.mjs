07ad019bbdc5eca5f8823f763f70c0e96a0cd782
Joe Acosta <joe.acosta@sephora.com>
2023-06-21 17:45:00 -0700

    shell file for uploading to astrometry

---
+++ xhrActions/astrometryCheck.mjs
@@ -0,0 +1,24 @@
+import {
+    login
+} from '#libs/astrometry/login.mjs';
+
+import {
+    upload
+} from '#libs/astrometry/fileupload.mjs';
+
+const apiKey = '';
+
+login(apiKey).then(session => {
+    if (session) {
+        const filepath = 'M3-20230603214015-tweaked.png';
+        upload(session, filepath).then(status => {
+            console.log(status);
+        }).catch(xerr => {
+            console.log(xerr);
+        });
+    } else {
+        console.log(res);
+    }
+}).catch(err => {
+    console.log(err);
+});
