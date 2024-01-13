1fc73d87e102cdaf21f06a59111907c6c41c9bd2
josepha48 <josepha48@gmail.com>
2024-01-13 12:43:17 -0800

    update to check histogram and dump results to console

--- scripts/siril_shell.sh
+++ scripts/siril_shell.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 #
-#  resample a file
-#  used to resample bin 2x2 to 1x1
-#  useful for lrgb with l in 1x1 and rgb in 2x2
+# get hisogram data files from a light frame
+# looking at histogram is ideal
+# sometimes histogram view is difficult
 #
 version=$(siril --version |awk '{print $2}')
 ext=fits
 FILE=$1
 SCRIPT=/tmp/script.ssf
 
-mkdir process
-mkdir out
+mkdir histoin
+mkdir histoout
+
 cp $FILE process/histo.fits
 echo "requires $version" > $SCRIPT
-echo "cd process" >> $SCRIPT
-echo "convert histo -debayer -out=../out" >> $SCRIPT
+echo "cd histoin" >> $SCRIPT
+echo "convert histo -debayer -out=../histoout" >> $SCRIPT
 echo "cd .." >> $SCRIPT
-echo "load out/histo_00001.fit" >> $SCRIPT
+echo "load histoout/histo_00001.fit" >> $SCRIPT
 echo "histo 0" >> $SCRIPT
 echo "histo 1" >> $SCRIPT
 echo "histo 2" >> $SCRIPT
@@ -24,4 +25,8 @@ echo "close" >> $SCRIPT
 
 siril-cli -s $SCRIPT
 
-rm $SCRIPT process/* out/*
+rm $SCRIPT histoin/* histoout/*
+
+# we now should have a histo_red.dat, histo_blue.dat, and histo_blue.dat
+# now run node program 
+node checkHist.js
