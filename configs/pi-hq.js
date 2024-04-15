157fa700bf727f83ec7ec854a30fe19703c043fb
josepha48 <josepha48@gmail.com>
2024-04-14 18:57:15 -0700

    add in video config for pi hq camera

---
+++ configs/pi-hq.js
@@ -0,0 +1,15 @@
+
+
+const videoSize = [
+'--width 320 --height 240'
+'--width 640 --height 480'
+'--width 800 --height 600',
+'--width 1280 --height 720',
+'--width 1920 --height 1080',
+'--width 2028 --height 1520',
+'--width 4056 --height 3040'
+];
+
+module.exports = {
+    videoSize
+}
