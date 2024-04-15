7d0b05a51f89717294438d566e1892754c4da7a6
Joe Acosta <joe.acosta@sephora.com>
2024-04-15 13:21:44 -0700

    CONFIG SUPPORT

--- configs/pi-v2.1.js
+++ configs/pi-v2.1.js
@@ -6,8 +6,8 @@ const videoSize = [
     '--width 800 --height 600',
     '--width 1280 --height 720',
     '--width 1920 --height 1080',
-    '--width 2320 --height 1736',
-    '--width 4640 --height 3472'
+    '--width 1640 --height 1232',
+    '--width 3280 --height 2464'
 ];
 
 module.exports = {
