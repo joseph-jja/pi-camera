4feb43d0670a07b581f5e729d1d65973a2459636
josepha48 <josepha48@gmail.com>
2024-04-14 18:59:21 -0700

    add in config for pivarity

---
+++ configs/pivarity-16mp.js
@@ -0,0 +1,15 @@
+
+
+const videoSize = [
+'--width 320 --height 240'
+'--width 640 --height 480'
+'--width 800 --height 600',
+'--width 1280 --height 720',
+'--width 1920 --height 1080',
+'--width 2320 --height 1736',
+'--width 4640 --height 3472'
+];
+
+module.exports = {
+    videoSize
+}
