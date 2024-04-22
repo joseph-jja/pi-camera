18918b23ef581a9c35fe6d59a388812fed5a9bf9
Joe Acosta <joe.acosta@sephora.com>
2024-04-22 11:18:21 -0700

    cleanup output

--- scripts/readFitsHeader.py
+++ scripts/readFitsHeader.py
@@ -11,10 +11,9 @@ hdr = hdul[0].header
 
 expTime = hdr['EXPTIME']
 
-print(expTime)
-print(hdr['TELESCOP'])
-print(hdr['XBINNING'])
-print(hdr['YBINNING'])
+print("Exposure time: ", expTime)
+print("Telescope: ", hdr['TELESCOP'])
+print("Binning: ", hdr['XBINNING'], "x", hdr['YBINNING'])
 
 hdul.close()
 
